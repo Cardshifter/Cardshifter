8891622b5e3fba71c7d2025ced03ba5c9125acbb
Simon Forsberg <zomis2k@hotmail.com>
2014-08-18 15:06:39 +0200

    Modified example test

--- cardshifter-core/src/test/java/com/cardshift/core/AppTest.java
+++ cardshifter-core/src/test/java/com/cardshift/core/AppTest.java
@@ -1,38 +1,12 @@
 package com.cardshift.core;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
 }
