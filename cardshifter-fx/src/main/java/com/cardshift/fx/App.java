fe3f320ec67dba4ea01e877674498d67bb355c9a
Simon Forsberg <zomis2k@hotmail.com>
2014-08-18 12:26:56 +0200

    Renamed package name

---
+++ cardshifter-fx/src/main/java/com/cardshift/fx/App.java
@@ -0,0 +1,13 @@
+package com.cardshift.fx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
