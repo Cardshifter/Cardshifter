15c5543f44c1e7d2d502371f63c9c1787fcae363
Simon Forsberg <zomis2k@hotmail.com>
2014-08-23 22:00:04 +0200

    Added Log4j configuration

--- cardshifter-server/src/main/java/com/cardshifter/server/main/ServerMain.java
+++ cardshifter-server/src/main/java/com/cardshifter/server/main/ServerMain.java
@@ -1,10 +1,13 @@
 package com.cardshifter.server.main;
 
+import org.apache.log4j.PropertyConfigurator;
+
 import com.cardshifter.server.model.MainServer;
 
 public class ServerMain {
 	
 	public static void main(String[] args) {
+		PropertyConfigurator.configure(ServerMain.class.getResource("log4j.properties"));
 		new MainServer().start();
 	}
 
