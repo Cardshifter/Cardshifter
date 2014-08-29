8570c8a0c43b33459a16b417f8735f484c4c9245
baz <baz@bazs-air.gateway.2wire.net>
2014-08-29 15:21:10 -0500

    moved the game to the document controller

--- cardshifter-fx/src/main/java/com/cardshifter/fx/App.java
+++ cardshifter-fx/src/main/java/com/cardshifter/fx/App.java
@@ -1,33 +1,19 @@
 package com.cardshifter.fx;
 
-import com.cardshifter.core.CommandLineOptions;
-import com.cardshifter.core.Game;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+//This class just loads the FXML document which initializes its DocumentController
+
 public class App extends Application {
-    
-    /*
-    private final Game game;
-    
-    public App(final Game game) {
-        this.game = Objects.requireNonNull(game, "game");;
-    }
-    */
     
     @Override
     public void start(Stage stage) throws Exception {     
-        //stage.setTitle("title");
         
-        this.startGame();
+        //stage.setTitle("title");
         
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
@@ -43,12 +29,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
-    public void startGame() throws Exception {
-        CommandLineOptions options = new CommandLineOptions();
-        InputStream file = options.getScript() == null ? Game.class.getResourceAsStream("start.lua") : new FileInputStream(new File(options.getScript()));
-	Game game = new Game(file, options.getRandom());
-	game.getEvents().startGame(game);
-    }
-    
 }
--- cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
+++ cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
@@ -1,11 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.cardshifter.fx;
 
+import com.cardshifter.core.CommandLineOptions;
+import com.cardshifter.core.Game;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -13,19 +13,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author baz
- */
 public class FXMLDocumentController implements Initializable {
+    
+    //need a forward declaration so that this is  global to the class
+    Game game;
+    
+    //I think this is a public constructor, this code initializes the Game
+    public FXMLDocumentController() throws Exception {
+        CommandLineOptions options = new CommandLineOptions();
+        InputStream file = options.getScript() == null ? Game.class.getResourceAsStream("start.lua") : new FileInputStream(new File(options.getScript()));
+        game = new Game(file, options.getRandom());
+    }
     
     @FXML
     private Label label;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        //System.out.println("You clicked me!");
+        label.setText("Starting Game");
+        game.getEvents().startGame(game);
     }
     
     @Override
