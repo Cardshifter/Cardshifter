a4b3b104c978056b25cfb4a1076e88dd47f61495
bazola <bazzybazz@gmail.com>
2014-09-03 10:20:17 -0500

    added button to play cards

--- cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
+++ cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
@@ -18,11 +18,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
@@ -166,7 +168,7 @@ public class FXMLDocumentController implements Initializable {
            
             //This gets the text from Lua and makes labels for each property
             int stringIndex = 0;
-            List<String> stringList = new ArrayList<String>();
+            List<String> stringList = new ArrayList<>();
             LuaTools.processLuaTable(card.data.checktable(), (k, v) -> stringList.add(k + ": " + v));
             for (String string : stringList) {
                 Group cardTextStrings = new Group();
@@ -179,6 +181,46 @@ public class FXMLDocumentController implements Initializable {
                 cardTextStrings.getChildren().add(cardStringLabel);
                 stringIndex++;
             }
+            
+            if (isCardActive == true) {
+                                //only make a button when the card is active
+                Button button = new Button();
+                //button.setStyle("-fx-background-color:transparent");
+                button.minWidth(100);
+                button.minHeight(100);
+                button.prefWidth(100);
+                button.prefHeight(100);
+                button.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println("Trying to Perform Action");
+                        Map<String, UsableAction> cardActions = card.getActions();
+                        for (UsableAction action : cardActions.values()) {
+                            if (action.isAllowed()) {
+				if (action instanceof TargetAction) {
+					TargetAction targetAction = (TargetAction) action;
+					List<Targetable> targets = targetAction.findTargets();
+					if (targets.isEmpty()) {
+						return;
+					}
+					
+					//int targetIndex = Integer.parseInt(input.nextLine());
+                                        int targetIndex = 0;
+					if (targetIndex < 0 || targetIndex >= actions.size()) {
+						return;
+					}
+					
+                                        //TODO: add a check to make sure the target is valid//
+					Targetable target = targets.get(targetIndex);
+					targetAction.perform(target);
+				}
+				else action.perform();
+                            }
+                        }
+                    }
+                });
+                cardGroup.getChildren().add(button);
+            }
 
             cardIndex++;
         }
@@ -186,7 +228,7 @@ public class FXMLDocumentController implements Initializable {
     private List<Card> getCurrentPlayerHand() {
         Player player = game.getFirstPlayer(); 
         Zone hand = (Zone)CoerceLuaToJava.coerce(player.data.get("hand"), Zone.class);
-        List<Card> cardsInHand = new ArrayList<Card>();
+        List<Card> cardsInHand = new ArrayList<>();
         hand.getCards().forEach(card -> {
             cardsInHand.add(card);
         });
