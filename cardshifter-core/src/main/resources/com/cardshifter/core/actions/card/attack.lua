e37178a80d43029024dcb0f1b9f4e8c98de6f44c
skiwi <frank_skiwi@hotmail.com>
2014-09-03 17:36:59 +0200

    Refactored out end turn, play, scrap, attack, enchant and enchant special actions out of the startup script.

---
+++ cardshifter-core/src/main/resources/com/cardshifter/core/actions/card/attack.lua
@@ -0,0 +1,67 @@
+local attack = {}
+
+function attack.isAllowed(card, action)
+	local currentPlayer = card:getGame():getCurrentPlayer()
+	if card:getOwner() ~= currentPlayer then
+		return false
+	end
+	if card:getZone() ~= currentPlayer.data.battlefield then
+		return false
+	end
+	if card.data.attacksAvailable <= 0 then
+		return false
+	end
+	if card.data.sickness > 0 then
+		return false
+	end
+	return true
+end
+
+function attack.perform(card, target, action)
+	card.data.attacksAvailable = card.data.attacksAvailable - 1
+	if target.data.cardType == 'Player' then
+		target.data.life = target.data.life - card.data.strength
+		if target.data.life <= 0 then
+			card:getGame():gameOver()
+		end
+		return true
+	end
+	target.data.health = target.data.health - card.data.strength
+	card.data.health = card.data.health - target.data.strength
+	
+	if target.data.health <= 0 then
+		local opp = target:getOwner()
+		-- All units have trample
+		-- Target health is negative so add that to opponent life
+		opp.data.life = opp.data.life + target.data.health
+		if opp.data.life <= 0 then
+			card:getGame():gameOver()
+		end
+		target:destroy()
+	end
+	if card.data.health <= 0 then
+		card:destroy()
+	end
+
+	return false
+end
+
+function attack.isTargetAllowed(card, target, action)
+	local currentPlayer = card:getGame():getCurrentPlayer()
+	local oppPlayer = currentPlayer:getNextPlayer()
+	local oppBattlefield = oppPlayer.data.battlefield
+	
+	if oppBattlefield:isEmpty() then
+		return target == oppPlayer
+	end
+	if target.data.cardType == 'Player' then
+		return false
+	end
+	if target:getZone() ~= oppBattlefield then
+		return false
+	end
+	print("Allowed to target unit in " .. target:getZone():toString() .. " Target is " .. target.data.cardType)
+	return true
+end
+
+return attack
--- cardshifter-core/src/main/resources/com/cardshifter/core/start.lua
+++ cardshifter-core/src/main/resources/com/cardshifter/core/start.lua
@@ -1,31 +1,32 @@
 -- Always name this function "startGame"
 function startGame(game)
-    print("test: " .. game:toString())
-    game:on('actionUsed', onActionUsed)
-    game:on('turnStart', onTurnStart)
+  local endturnAction = require "src/main/resources/com/cardshifter/core/actions/player/endturn"
+
+  game:on('actionUsed', onActionUsed)
+  game:on('turnStart', onTurnStart)
 
 	local numPlayers = game:getPlayers():size()
 	for i=0,numPlayers-1 do
 		local player = game:getPlayer(i)
 		print("Player: " .. player:toString())
 		
-		player:addAction("End Turn", allowNextTurn, endTurn)
+		player:addAction("End Turn", endturnAction.isAllowed, endturnAction.perform)
 		
-	    player.data.life = 10
-	    player.data.mana = 0
-	    player.data.manaMax = 0
-	    player.data.scrap = 0
-	    player.data.cardType = 'Player'
-	    local field = game:createZone(player, "Battlefield")
-	    field:setGloballyKnown(true)
-	    player.data.battlefield = field
-		
-	    local deck = game:createZone(player, "Deck")
-	    player.data.deck = deck
-		
-	    local hand = game:createZone(player, "Hand")
-	    hand:setKnown(player, true)
-	    player.data.hand = hand
+    player.data.life = 10
+    player.data.mana = 0
+    player.data.manaMax = 0
+    player.data.scrap = 0
+    player.data.cardType = 'Player'
+    local field = game:createZone(player, "Battlefield")
+    field:setGloballyKnown(true)
+    player.data.battlefield = field
+
+    local deck = game:createZone(player, "Deck")
+    player.data.deck = deck
+
+    local hand = game:createZone(player, "Hand")
+    hand:setKnown(player, true)
+    player.data.hand = hand
 		
 		for i=1,4 do
 			local card
@@ -60,9 +61,11 @@ function startGame(game)
 end
 
 function createSpecialEnchantment(deck, strength, health, cost)
+  local enchantspecialAction = require "src/main/resources/com/cardshifter/core/actions/card/enchantspecial"
+  
     -- A special enchantment can only target a creature that has been enchanted already
 	local card = deck:createCardOnBottom()
-	card:addTargetAction("Enchant", enchAllowed, enchCard, enchSpecialTargetAllowed)
+	card:addTargetAction("Enchant", enchantspecialAction.isAllowed, enchantspecialAction.perform, enchantspecialAction.isTargetAllowed)
 	card.data.manaCost = 0
 	card.data.scrapCost = cost
 	card.data.enchStrength = strength
@@ -72,9 +75,11 @@ function createSpecialEnchantment(deck, strength, health, cost)
 end
 
 function createEnchantment(deck, strength, health, cost)
+  local enchantAction = require "src/main/resources/com/cardshifter/core/actions/card/enchant"
+  
     -- Can only target creatureType == 'Bio'
 	local card = deck:createCardOnBottom()
-	card:addTargetAction("Enchant", enchAllowed, enchCard, enchTargetAllowed)
+	card:addTargetAction("Enchant", enchantAction.isAllowed, enchantAction.perform, enchantAction.isTargetAllowed)
 	card.data.manaCost = 0
 	card.data.scrapCost = cost
 	card.data.enchStrength = strength
@@ -84,10 +89,14 @@ function createEnchantment(deck, strength, health, cost)
 end
 
 function createCreature(deck, cost, strength, health, creatureType)
+  local playAction = require "src/main/resources/com/cardshifter/core/actions/card/play"
+  local attackAction = require "src/main/resources/com/cardshifter/core/actions/card/attack"
+  local scrapAction = require "src/main/resources/com/cardshifter/core/actions/card/scrap"
+  
 	local card = deck:createCardOnBottom()
-	card:addAction("Play", playAllowed, playCard)
-	card:addTargetAction("Attack", attackAllowed, attackCard, attackTargetAllowed)
-	card:addAction("Scrap", scrapAllowed, scrapCard)
+	card:addAction("Play", playAction.isAllowed, playAction.perform)
+	card:addTargetAction("Attack", attackAction.isAllowed, attackAction.perform, attackAction.isTargetAllowed)
+	card:addAction("Scrap", scrapAction.isAllowed, scrapAction.perform)
 	card.data.manaCost = cost
 	card.data.strength = strength
 	card.data.health = health
@@ -130,157 +139,157 @@ function drawCard(player)
 	card:moveToBottomOf(player.data.hand)
 end
 
-function playAllowed(card)
-	local currPlayer = card:getGame():getCurrentPlayer()
-	if card:getOwner() ~= currPlayer then
-		return false
-	end
-	if card:getZone() ~= currPlayer.data.hand then
-		return false
-	end
-	if card.data.manaCost > currPlayer.data.mana then
-		return false
-	end
-	return true
-end
+--function playAllowed(card)
+--	local currPlayer = card:getGame():getCurrentPlayer()
+--	if card:getOwner() ~= currPlayer then
+--		return false
+--	end
+--	if card:getZone() ~= currPlayer.data.hand then
+--		return false
+--	end
+--	if card.data.manaCost > currPlayer.data.mana then
+--		return false
+--	end
+--	return true
+--end
+--
+--function playCard(card)
+--    local owner = card:getOwner()
+--	card:moveToBottomOf(owner.data.battlefield)
+--	
+--	owner.data.mana = owner.data.mana - card.data.manaCost
+--end
 
-function playCard(card)
-    local owner = card:getOwner()
-	card:moveToBottomOf(owner.data.battlefield)
-	
-	owner.data.mana = owner.data.mana - card.data.manaCost
-end
+--function attackTargetAllowed(card, target, action)
+--	local currPlayer = card:getGame():getCurrentPlayer()
+--	local oppPlayer = currPlayer:getNextPlayer()
+--	local oppBattlefield = oppPlayer.data.battlefield
+--	
+--	if oppBattlefield:isEmpty() then
+--		return target == oppPlayer
+--	end
+--	if target.data.cardType == 'Player' then
+--		return false
+--	end
+--	if target:getZone() ~= oppBattlefield then
+--		return false
+--	end
+--	print("Allowed to target unit in " .. target:getZone():toString() .. " Target is " .. target.data.cardType)
+--	return true
+--end
+--
+--function attackAllowed(card, action)
+--	local currPlayer = card:getGame():getCurrentPlayer()
+--	if card:getOwner() ~= currPlayer then
+--		return false
+--	end
+--	if card:getZone() ~= currPlayer.data.battlefield then
+--		return false
+--	end
+--	if card.data.attacksAvailable <= 0 then
+--		return false
+--	end
+--	if card.data.sickness > 0 then
+--		return false
+--	end
+--	return true
+--end
+--
+--function attackCard(card, target, action)
+--	card.data.attacksAvailable = card.data.attacksAvailable - 1
+--	if target.data.cardType == 'Player' then
+--		target.data.life = target.data.life - card.data.strength
+--		if target.data.life <= 0 then
+--			card:getGame():gameOver()
+--		end
+--		return true
+--	end
+--	target.data.health = target.data.health - card.data.strength
+--	card.data.health = card.data.health - target.data.strength
+--	
+--	if target.data.health <= 0 then
+--		local opp = target:getOwner()
+--		-- All units have trample
+--		-- Target health is negative so add that to opponent life
+--		opp.data.life = opp.data.life + target.data.health
+--		if opp.data.life <= 0 then
+--			card:getGame():gameOver()
+--		end
+--		target:destroy()
+--	end
+--	if card.data.health <= 0 then
+--		card:destroy()
+--	end
+--
+--	return false
+--end
 
-function attackTargetAllowed(card, target, action)
-	local currPlayer = card:getGame():getCurrentPlayer()
-	local oppPlayer = currPlayer:getNextPlayer()
-	local oppBattlefield = oppPlayer.data.battlefield
-	
-	if oppBattlefield:isEmpty() then
-		return target == oppPlayer
-	end
-	if target.data.cardType == 'Player' then
-		return false
-	end
-	if target:getZone() ~= oppBattlefield then
-		return false
-	end
-	print("Allowed to target unit in " .. target:getZone():toString() .. " Target is " .. target.data.cardType)
-	return true
-end
+--function enchAllowed(card)
+--	if not playAllowed(card) then
+--		return false
+--	end
+--	local currPlayer = card:getGame():getCurrentPlayer()
+--	if card.data.cardType ~= 'Enchantment' then
+--		return false
+--	end
+--	if card.data.scrapCost > currPlayer.data.scrap then
+--		return false
+--	end
+--	return true
+--end
+--
+--function enchSpecialTargetAllowed(source, target, action)
+--	if target.data.cardType ~= 'Creature' then
+--		return false
+--	end
+----	if table.getn(target.data.enchantments) == 0 then
+--	if target.data.enchantments <= 0 then
+--		return false
+--	end
+--	return true
+--end
+--
+--function enchTargetAllowed(source, target, action)
+--	if target.data.cardType ~= 'Creature' then
+--		return false
+--	end
+--	if target.data.creatureType ~= 'Bio' then
+--		return false
+--	end
+--	return true
+--end
+--
+--function enchCard(card, target, action)
+--	target.data.enchantments = target.data.enchantments + 1
+--	target.data.health = target.data.health + card.data.enchHealth
+--	target.data.strength = target.data.strength + card.data.enchStrength
+--	local owner = card:getOwner()
+--	owner.data.scrap = owner.data.scrap - card.data.scrapCost 
+--	card:destroy()
+--end
 
-function attackAllowed(card, action)
-	local currPlayer = card:getGame():getCurrentPlayer()
-	if card:getOwner() ~= currPlayer then
-		return false
-	end
-	if card:getZone() ~= currPlayer.data.battlefield then
-		return false
-	end
-	if card.data.attacksAvailable <= 0 then
-		return false
-	end
-	if card.data.sickness > 0 then
-		return false
-	end
-	return true
-end
+--function scrapAllowed(card)
+--	local currPlayer = card:getGame():getCurrentPlayer()
+--	if card:getOwner() ~= currPlayer then
+--		return false
+--	end
+--	if card:getZone() ~= currPlayer.data.battlefield then
+--		return false
+--	end
+--	return true
+--end
+--
+--function scrapCard(card)
+--	local owner = card:getOwner()
+--	card:destroy()
+--	owner.data.scrap = owner.data.scrap + 1
+--end
 
-function attackCard(card, target, action)
-	card.data.attacksAvailable = card.data.attacksAvailable - 1
-	if target.data.cardType == 'Player' then
-		target.data.life = target.data.life - card.data.strength
-		if target.data.life <= 0 then
-			card:getGame():gameOver()
-		end
-		return true
-	end
-	target.data.health = target.data.health - card.data.strength
-	card.data.health = card.data.health - target.data.strength
-	
-	if target.data.health <= 0 then
-		local opp = target:getOwner()
-		-- All units have trample
-		-- Target health is negative so add that to opponent life
-		opp.data.life = opp.data.life + target.data.health
-		if opp.data.life <= 0 then
-			card:getGame():gameOver()
-		end
-		target:destroy()
-	end
-	if card.data.health <= 0 then
-		card:destroy()
-	end
-
-	return false
-end
-
-function enchAllowed(card)
-	if not playAllowed(card) then
-		return false
-	end
-	local currPlayer = card:getGame():getCurrentPlayer()
-	if card.data.cardType ~= 'Enchantment' then
-		return false
-	end
-	if card.data.scrapCost > currPlayer.data.scrap then
-		return false
-	end
-	return true
-end
-
-function enchSpecialTargetAllowed(source, target, action)
-	if target.data.cardType ~= 'Creature' then
-		return false
-	end
---	if table.getn(target.data.enchantments) == 0 then
-	if target.data.enchantments <= 0 then
-		return false
-	end
-	return true
-end
-
-function enchTargetAllowed(source, target, action)
-	if target.data.cardType ~= 'Creature' then
-		return false
-	end
-	if target.data.creatureType ~= 'Bio' then
-		return false
-	end
-	return true
-end
-
-function enchCard(card, target, action)
-	target.data.enchantments = target.data.enchantments + 1
-	target.data.health = target.data.health + card.data.enchHealth
-	target.data.strength = target.data.strength + card.data.enchStrength
-	local owner = card:getOwner()
-	owner.data.scrap = owner.data.scrap - card.data.scrapCost 
-	card:destroy()
-end
-
-function scrapAllowed(card)
-	local currPlayer = card:getGame():getCurrentPlayer()
-	if card:getOwner() ~= currPlayer then
-		return false
-	end
-	if card:getZone() ~= currPlayer.data.battlefield then
-		return false
-	end
-	return true
-end
-
-function scrapCard(card)
-	local owner = card:getOwner()
-	card:destroy()
-	owner.data.scrap = owner.data.scrap + 1
-end
-
-function endTurn(player)
-	local game = player:getGame()
-	game:nextTurn()
-end
-
-function allowNextTurn(player)
-	return player:getGame():getCurrentPlayer() == player
-end
+--function endTurn(player)
+--	local game = player:getGame()
+--	game:nextTurn()
+--end
+--
+--function allowNextTurn(player)
+--	return player:getGame():getCurrentPlayer() == player
+--end
