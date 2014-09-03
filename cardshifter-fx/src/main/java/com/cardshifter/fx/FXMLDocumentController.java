b930ea9e441e5d084c281ea92c0786e1fe91d5c8
bazola <bazzybazz@gmail.com>
2014-09-03 12:47:25 -0500

    refactored to create CardNode class

--- cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
+++ cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
@@ -3,12 +3,7 @@ package com.cardshifter.fx;
 import com.cardshifter.core.CommandLineOptions;
 import com.cardshifter.core.Game;
 import com.cardshifter.core.Card;
-import com.cardshifter.core.CardAction;
-import com.cardshifter.core.LuaTools;
 import com.cardshifter.core.Player;
-import com.cardshifter.core.TargetAction;
-import com.cardshifter.core.Targetable;
-import com.cardshifter.core.UsableAction;
 import com.cardshifter.core.Zone;
 
 import java.io.File;
@@ -18,13 +13,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -113,112 +105,19 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Pane player01Pane;
     private void renderPlayerHand() {
-        //First get the list of Card objects
         List<Card> cardsInHand = this.getCurrentPlayerHand();
-        
-        List<Card> cardsWithActions = new ArrayList<>();
-        List<UsableAction> actions = game.getAllActions().stream().filter(action -> 
-                action.isAllowed()).collect(Collectors.toList());
-        for (UsableAction action : actions) {
-            if (action instanceof CardAction) {
-                Card card = ((CardAction)action).getCard();
-                cardsWithActions.add(card);
-            }
-        }
-        
-        //Create a group for each card for positioning
-        //Another group is needed for each element in the LuaTable
+
         int cardIndex = 0;
         for (Card card : cardsInHand) {
             System.out.println("found a card");
             
-            //check if the card is active
-            boolean isCardActive = false;
-            for (Card cardWithAction : cardsWithActions) {
-                if (card == cardWithAction) {
-                    isCardActive = true;
-                    //do a test to see if another break is needed
-                    break;
-                }
-            }
-            
-            Group cardGroup = new Group();
+            CardNode cardNode = new CardNode(100, 100, "testName", card);
+            Group cardGroup = cardNode.getCardGroup();
+            cardGroup.setAutoSizeChildren(true); //NEW
             cardGroup.setId(String.format("player01card%d", cardIndex));
             cardGroup.setTranslateX(cardIndex * 165);
             player01Pane.getChildren().add(cardGroup);
             
-            Rectangle activeBackground = new Rectangle(-10,-10,170,240);
-            activeBackground.setFill(Color.BLACK);
-            if(isCardActive == true) {
-                activeBackground.setFill(Color.YELLOW);
-            }
-            cardGroup.getChildren().add(activeBackground);
-            
-            Rectangle cardBack = new Rectangle(0,0,150,220);
-            cardBack.setFill(Color.FIREBRICK);
-            cardGroup.getChildren().add(cardBack);
-
-            Label cardIdLabel = new Label();
-            cardIdLabel.setText(String.format("CardID = %d", card.getId()));
-            cardIdLabel.setTextFill(Color.WHITE);
-            cardGroup.getChildren().add(cardIdLabel);
-           
-            //This gets the text from Lua and makes labels for each property
-            int stringIndex = 0;
-            List<String> stringList = new ArrayList<>();
-            LuaTools.processLuaTable(card.data.checktable(), (k, v) -> stringList.add(k + ": " + v));
-            for (String string : stringList) {
-                Group cardTextStrings = new Group();
-                cardTextStrings.setTranslateY(25 + (stringIndex * 25));
-                cardGroup.getChildren().add(cardTextStrings);
-            
-                Label cardStringLabel = new Label();
-                cardStringLabel.setText(string);
-                cardStringLabel.setTextFill(Color.WHITE);
-                cardTextStrings.getChildren().add(cardStringLabel);
-                stringIndex++;
-            }
-            
-            if (isCardActive == true) {
-                //only make a button when the card is active
-                Button button = new Button();
-                //button.setStyle("-fx-background-color:transparent");
-                button.minWidth(100);
-                button.minHeight(100);
-                button.prefWidth(100);
-                button.prefHeight(100);
-                button.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        System.out.println("Trying to Perform Action");
-                        List<UsableAction> cardActions = card.getActions().values().stream().filter(UsableAction::isAllowed).collect(Collectors.toList());
-                        for (UsableAction action : cardActions) {
-                            if (action.isAllowed()) {
-				if (action instanceof TargetAction) {
-					TargetAction targetAction = (TargetAction) action;
-					List<Targetable> targets = targetAction.findTargets();
-					if (targets.isEmpty()) {
-						return;
-					}
-					
-					//int targetIndex = Integer.parseInt(input.nextLine());
-                                        int targetIndex = 0;
-					if (targetIndex < 0 || targetIndex >= actions.size()) {
-						return;
-					}
-					
-                                        //TODO: add a check to make sure the target is valid//
-					Targetable target = targets.get(targetIndex);
-					targetAction.perform(target);
-				}
-				else action.perform();
-                            }
-                        }
-                    }
-                });
-                cardGroup.getChildren().add(button);
-            }
-
             cardIndex++;
         }
     }
