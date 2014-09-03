2dd1d99cdf75bc143e3df55a984024f58ba037aa
bazola <bazzybazz@gmail.com>
2014-09-03 11:04:39 -0500

    fixed action bug, started refactoring

--- cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
+++ cardshifter-fx/src/main/java/com/cardshifter/fx/FXMLDocumentController.java
@@ -17,11 +17,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.Objects;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -32,7 +28,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-
 import javafx.scene.shape.QuadCurve;
 import javafx.scene.shape.Rectangle;
 
@@ -40,12 +35,11 @@ import org.luaj.vm2.lib.jse.CoerceLuaToJava;
 
 public class FXMLDocumentController implements Initializable {
     
+    //INITIAL GAME SETUP
     //need a forward declaration so that this is  global to the class
     Game game;
-    
     //hack to make the buttons work properly
     private boolean gameHasStarted = false;
-    
     //I think this is a public constructor, this code initializes the Game
     public FXMLDocumentController() throws Exception {
         CommandLineOptions options = new CommandLineOptions();
@@ -53,28 +47,33 @@ public class FXMLDocumentController implements Initializable {
         game = new Game(file, options.getRandom());
     }
     
+    //START GAME BUTTON
     @FXML
-    private Label label;
-    
+    private Label startGameLabel;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        //System.out.println("You clicked me!");
+    private void startGameButtonAction(ActionEvent event) {
         if (gameHasStarted == false) {
-            label.setText("Starting Game");
+            startGameLabel.setText("Starting Game");
             game.getEvents().startGame(game);
             gameHasStarted = true;
             this.renderHands();
         }
     }
-    
+   
+    //UPDATE LOOP
+    private void render() {
+        this.renderHands();
+    }
     private void renderHands() {
         this.renderPlayerHand();
         this.renderOpponentHand();
     }
     
+    //TODO: Convert this to mana totals for players, and only increment every play rotation
+    //TURN LABEL
     @FXML
     private Label turnLabel;
-    
+    //ADVANCE TURNS
     @FXML
     private void handleTurnButtonAction(ActionEvent event) {
         if (gameHasStarted == true) {
@@ -84,10 +83,7 @@ public class FXMLDocumentController implements Initializable {
         }
     }
     
-    @FXML
-    private QuadCurve handGuide;
-    
-    //NOT YET FINISHED
+    //RENDER PLAYER 2 CARD BACKS
     @FXML
     Pane player02Pane;
     private void renderOpponentHand() {
@@ -112,6 +108,7 @@ public class FXMLDocumentController implements Initializable {
         return cardsInHand.size();
     }
     
+    //RENDER PLAYER ONE CARDS
     /*BIG TO DO: Make all dimensions relative to the Pane size or Screen size*/
     @FXML
     private Pane player01Pane;
@@ -183,7 +180,7 @@ public class FXMLDocumentController implements Initializable {
             }
             
             if (isCardActive == true) {
-                                //only make a button when the card is active
+                //only make a button when the card is active
                 Button button = new Button();
                 //button.setStyle("-fx-background-color:transparent");
                 button.minWidth(100);
@@ -194,8 +191,8 @@ public class FXMLDocumentController implements Initializable {
                     @Override
                     public void handle(ActionEvent event) {
                         System.out.println("Trying to Perform Action");
-                        Map<String, UsableAction> cardActions = card.getActions();
-                        for (UsableAction action : cardActions.values()) {
+                        List<UsableAction> cardActions = card.getActions().values().stream().filter(UsableAction::isAllowed).collect(Collectors.toList());
+                        for (UsableAction action : cardActions) {
                             if (action.isAllowed()) {
 				if (action instanceof TargetAction) {
 					TargetAction targetAction = (TargetAction) action;
@@ -235,108 +232,13 @@ public class FXMLDocumentController implements Initializable {
         return cardsInHand;
     }
     
+    //BOILERPLATE
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }       
     
-  
-    public void play() {
-        Scanner input = new Scanner(System.in);
-        while (!game.isGameOver()) {
-            outputGameState();
-            List<UsableAction> actions = game.getAllActions().stream().filter(action -> action.isAllowed()).collect(Collectors.toList());
-            outputList(actions);
-			
-            String in = input.nextLine();
-            if (in.equals("exit")) {
-                break;
-            }
-			
-            handleActionInput(actions, in, input);
-            }
-	//print("--------------------------------------------");
-	outputGameState();
-	//print("Game over!");
-    }
-    
-    private void handleActionInput(final List<UsableAction> actions, final String in, Scanner input) {
-	Objects.requireNonNull(actions, "actions");
-	Objects.requireNonNull(in, "in");
-	//print("Choose an action:");
-		
-	try {
-            int value = Integer.parseInt(in);
-            if (value < 0 || value >= actions.size()) {
-		//print("Action index out of range: " + value);
-		return;
-            }
-			
-            UsableAction action = actions.get(value);
-            //print("Action " + action);
-            if (action.isAllowed()) {
-		if (action instanceof TargetAction) {
-                    TargetAction targetAction = (TargetAction) action;
-                    List<Targetable> targets = targetAction.findTargets();
-                    if (targets.isEmpty()) {
-			//print("No available targets for action");
-			return;
-                    }
-					
-                    outputList(targets);
-                    //print("Enter target index:");
-                    int targetIndex = Integer.parseInt(input.nextLine());
-                    if (value < 0 || value >= actions.size()) {
-                        //print("Target index out of range: " + value);
-                        return;
-                    }
-					
-                    Targetable target = targets.get(targetIndex);
-                    targetAction.perform(target);
-		}
-		else action.perform();
-		//print("Action performed");
-            }
-            else {
-		//print("Action is not allowed");
-            }
-        }
-	catch (NumberFormatException ex) {
-            //print("Illegal action index: " + in);
-	}
-    }
-
-    private void outputList(final List<?> actions) {
-        Objects.requireNonNull(actions, "actions");
-	//print("------------------");
-	ListIterator<?> it = actions.listIterator();
-	while (it.hasNext()) {
-            //print(it.nextIndex() + ": " + it.next());
-	}
-    }
-    
-    private void outputGameState() {
-	//print("------------------");
-	//print(this.game);
-	for (Player player : game.getPlayers()) {
-            //print(player);
-            //player.getActions().values().forEach(action -> print(4, "Action: " + action));
-            //printLua(4, player.data); // TODO: Some LuaData should probably be hidden from other players, or even from self.
-	}
-		
-	for (Zone zone : game.getZones()) {
-            //print(zone);
-            if (zone.isKnownToPlayer(game.getCurrentPlayer())) {
-		zone.getCards().forEach(card -> {
-                    //print(4, card);
-                    //card.getActions().values().forEach(action -> print(8, "Action: " + action));
-                    //printLua(8, card.data);
-		});
-            }
-	}
-    }
-
-    
-    
-    
+    //NOT YET USED
+    @FXML
+    private QuadCurve handGuide;
 }
