2aa85f76c87d0cd42d2c91eaf1c276bd00362814
Simon Forsberg <zomis2k@hotmail.com>
2014-09-10 20:12:37 +0200

    fixed #13 also on server

--- cardshifter-server/src/main/resources/com/cardshifter/core/actions/card/enchantspecial.lua
+++ cardshifter-server/src/main/resources/com/cardshifter/core/actions/card/enchantspecial.lua
@@ -29,9 +29,14 @@ function enchantspecial.isTargetAllowed(card, target, action)
 	if target.data.cardType ~= 'Creature' then
 		return false
 	end
---	if table.getn(target.data.enchantments) == 0 then
 	if target.data.enchantments <= 0 then
 		return false
+	end
+	if target:getOwner() ~= card:getOwner() then
+	  return false
+	end
+	if target:getZone() ~= card:getOwner().data.battlefield then
+	  return false
 	end
 	return true
 end
