7e9239c764f9a8c042f4af086be79a3243135a1e
Simon Forsberg <zomis2k@hotmail.com>
2014-09-15 22:57:05 +0200

    Corrected GameOverIfNoHealth system

--- cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/systems/GameOverIfNoHealth.java
+++ cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/systems/GameOverIfNoHealth.java
@@ -2,13 +2,29 @@ package net.zomis.cardshifter.ecs.systems;
 
 import net.zomis.cardshifter.ecs.base.ECSGame;
 import net.zomis.cardshifter.ecs.base.System;
+import net.zomis.cardshifter.ecs.components.PlayerComponent;
+import net.zomis.cardshifter.ecs.resources.ECSResource;
 import net.zomis.cardshifter.ecs.resources.ResourceValueChange;
 
 public class GameOverIfNoHealth implements System {
 
+	private final ECSResource resource;
+
+	public GameOverIfNoHealth(ECSResource resource) {
+		this.resource = resource;
+	}
+	
 	@Override
 	public void startGame(ECSGame game) {
-		game.getEvents().registerHandlerAfter(ResourceValueChange.class, event -> game.endGame());
+		game.getEvents().registerHandlerAfter(ResourceValueChange.class, this::endGame);
+	}
+	
+	private void endGame(ResourceValueChange event) {
+		if (event.getResource() == this.resource) {
+			if (event.getNewValue() <= 0 && event.getEntity().hasComponent(PlayerComponent.class)) {
+				event.getEntity().getGame().endGame();
+			}
+		}
 	}
 
 }
