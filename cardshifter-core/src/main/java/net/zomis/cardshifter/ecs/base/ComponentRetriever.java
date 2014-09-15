647e2aa920c27c48f488ff6fe30c2cc6ce354aa5
Simon Forsberg <zomis2k@hotmail.com>
2014-09-15 22:56:14 +0200

    ComponentRetriever for when there is only one such component in the game

--- cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/base/ComponentRetriever.java
+++ cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/base/ComponentRetriever.java
@@ -1,5 +1,7 @@
 package net.zomis.cardshifter.ecs.base;
 
+import java.util.Set;
+
 public class ComponentRetriever<T extends Component> {
 
 	private final Class<T> clazz;
@@ -18,6 +20,26 @@ public class ComponentRetriever<T extends Component> {
 
 	public static <T extends Component> ComponentRetriever<T> retreiverFor(Class<T> clazz) {
 		return new ComponentRetriever<>(clazz);
+	}
+
+	public static <T extends Component> ComponentRetriever<T> singleton(Class<T> class1) {
+		return new ComponentRetriever<T>(null) {
+			
+			@Override
+			public boolean has(Entity entity) {
+				return get(entity) != null;
+			}
+			
+			@Override
+			public T get(Entity entity) {
+				Set<Entity> all = entity.getGame().getEntitiesWithComponent(class1);
+				if (all.size() != 1) {
+					throw new IllegalStateException();
+				}
+				return all.iterator().next().getComponent(class1);
+			}
+			
+		};
 	}
 	
 }
