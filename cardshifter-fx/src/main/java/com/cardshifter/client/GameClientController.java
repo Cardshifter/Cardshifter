91d846292fc9b773af735dd02ef598110b634f3b
bazola <bazzybazz@gmail.com>
2014-09-16 12:54:35 -0500

    created basic structure for the game loop of the client

--- cardshifter-fx/src/main/java/com/cardshifter/client/GameClientController.java
+++ cardshifter-fx/src/main/java/com/cardshifter/client/GameClientController.java
@@ -5,8 +5,8 @@ import com.cardshifter.server.incoming.RequestTargetsMessage;
 import com.cardshifter.server.incoming.StartGameRequest;
 import com.cardshifter.server.incoming.UseAbilityMessage;
 import com.cardshifter.server.messages.Message;
+import com.cardshifter.server.outgoing.CardInfoMessage;
 import com.cardshifter.server.outgoing.NewGameMessage;
-import com.cardshifter.server.outgoing.ResetAvailableActionsMessage;
 import com.cardshifter.server.outgoing.UseableActionMessage;
 import com.cardshifter.server.outgoing.WaitMessage;
 import com.cardshifter.server.outgoing.WelcomeMessage;
@@ -24,40 +24,46 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
 
 public class GameClientController {
+	
 	@FXML
 	Label serverMessage;
 	@FXML
 	AnchorPane rootPane;
+	@FXML
+	ListView serverMessages;
 	
 	private Socket socket;
 	private InputStream in;
 	private OutputStream out;
 	private final ObjectMapper mapper = new ObjectMapper();
+	
 	private final BlockingQueue<Message> messages = new LinkedBlockingQueue<>();
 	private final List<UseableActionMessage> actions = Collections.synchronizedList(new ArrayList<>());
-	private int gameId;
+	private final List<CardInfoMessage> cards = Collections.synchronizedList(new ArrayList<>());
+	private final List<Message> genericMessages = Collections.synchronizedList(new ArrayList<>());
+	private final List<UseableActionMessage> actionsForServer = Collections.synchronizedList(new ArrayList<>());
 	
+	private int gameId;
 	private String ipAddress;
 	private int port;
 	
+	/////////INITIALIZATION///////////////
+	//this is passed into the object after it is created by the FXML document
 	public void acceptIPAndPort(String ipAddress, int port) {
 		this.ipAddress = ipAddress;
 		this.port = port;
 	}
-	
+	//this is called on the object from the Game launcher before the scene is displayed
 	public void connectToGame() {
 		try {
 			this.socket = new Socket(this.ipAddress, this.port);
@@ -76,19 +82,20 @@ public class GameClientController {
 			e.printStackTrace();
 		}
 	}
-	
+	//this method only runs once at the start
 	public void play() {
-		System.out.println("Enter your name: ");
-//		String name = input.nextLine();
 		String name = "Player" + new Random().nextInt(100);
 		this.send(new LoginMessage(name));
 		
 		try {
 			WelcomeMessage response = (WelcomeMessage) messages.take();
-			System.out.println(response.getMessage());
 			if (!response.isOK()) {
 				return;
 			}
+			
+			//display the welcome message on the screen
+			Platform.runLater(() -> serverMessage.setText(response.getMessage()));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -97,92 +104,105 @@ public class GameClientController {
 		
 		try {
 			Message message = messages.take();
-			if (message instanceof WaitMessage) {
-				System.out.println(((WaitMessage) message).getMessage());
+			if (message instanceof WaitMessage) {	
 				
+				//display the wait message on the screen
 				Platform.runLater(() -> serverMessage.setText(((WaitMessage)message).getMessage()));
 
 				NewGameMessage game = (NewGameMessage) messages.take();
-				this.playLoop(game);
+				this.playLoop();
 			}
 			else {
-				this.playLoop((NewGameMessage) message);
+				this.playLoop();
 			}
+			this.gameId = ((NewGameMessage)message).getGameId();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private void playLoop(NewGameMessage game) throws JsonParseException, JsonMappingException, IOException {
-		System.out.printf("Game id %d. You are player index %d.%n", game.getGameId(), game.getPlayerIndex());
-		this.gameId = game.getGameId();
+	///////////UPDATE LOOP////////////////
+	//this runs continuously once it starts, gets messages from the server
+	private void listen() {
 		while (true) {
-			System.out.println("Waiting for input...");
-			
-			// TODO: Empty messages, perhaps do something with some of them as well...
-//			try {
-//				while (true) {
-//					// 
-//					messages.poll(500, TimeUnit.MILLISECONDS);
-//				}
-//			} catch (InterruptedException e) {
-//			}
-			
-			outputList(actions);
-			if (actions.isEmpty()) {
-				System.out.println("No available actions to perform right now.");
-			}
-			
-			/*
-			String inputLine = input.nextLine();
-			if (inputLine.equals("exit")) {
-				break;
-			}
-			*/
-			
 			try {
-				int actionIndex = 0;
-				UseableActionMessage action = actions.get(actionIndex);
+				MappingIterator<Message> values = mapper.readValues(new JsonFactory().createParser(this.in), Message.class);
+				if (values.hasNextValue()) {
+					this.collectMessagesFromServer(values);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			this.update();
+		}
+	}
+	private void collectMessagesFromServer(MappingIterator<Message> values) {
+		while(values.hasNext()) {
+			messages.offer(values.next());
+		}
+	}
+	
+	private void update() {
+		try {
+			this.playLoop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		this.sortMessagesFromServer();
+		this.render();
+	}
+	private void playLoop() throws JsonParseException, JsonMappingException, IOException {
+		if (actionsForServer.isEmpty()) {
+			return;
+		} else {
+			try {
+				UseableActionMessage action = actionsForServer.get(0);
 				if (action.isTargetRequired()) {
 					this.send(new RequestTargetsMessage(gameId, action.getId(), action.getAction()));
 				}
 				else {
 					this.send(new UseAbilityMessage(gameId, action.getId(), action.getAction(), action.getTargetId()));
 				}
-			}
-			catch (NumberFormatException | IndexOutOfBoundsException ex) {
+			} catch (NumberFormatException | IndexOutOfBoundsException ex) {
 				System.out.println("Not a valid action");
 			}
 		}
 		//print("--------------------------------------------");
 		//print("Game over!");
 	}
-	
-	private void listen() {
-		while (true) {
-			try {
-				Message mess = null;
-				System.out.println("Start loop");
-				MappingIterator<Message> values = mapper.readValues(new JsonFactory().createParser(this.in), Message.class);
-				while (values.hasNext()) {
-					mess = values.next();
-					System.out.println("iterator: " + mess);
-					messages.offer(mess);
-					if (mess instanceof ResetAvailableActionsMessage) {
-						actions.clear();
-					}
-					if (mess instanceof UseableActionMessage) {
-						System.out.println("New Action Available: " + actions.size() + " - " + mess);
-						actions.add((UseableActionMessage) mess);
-					}
-				}
-				System.out.println("End of loop, mess is " + mess);
-			} catch (IOException e) {
-				e.printStackTrace();
+	private void sortMessagesFromServer() {
+		while(!messages.isEmpty()) {
+			Message message = messages.remove();
+			if (message instanceof UseableActionMessage) {
+				actions.add((UseableActionMessage)message);
+			} else if (message instanceof CardInfoMessage) {
+				cards.add((CardInfoMessage)message);
+			} else {
+				genericMessages.add(message);
 			}
 		}
 	}
+	private void render() {
+		for (UseableActionMessage message : actions) {
+			//create a box for each usable action
+		}
+		actions.clear();
+		
+		for (CardInfoMessage message : cards) {
+			//render cards to their zones
+		}
+		cards.clear();
+		
+		List<String> listViewContent = new ArrayList<>();
+		for (Message message : genericMessages) {
+			listViewContent.add(message.toString());
+		}
+		serverMessages.getItems().setAll(listViewContent);
+		genericMessages.clear();
+	}
 	
+	/////////COMMUNICATE WITH SERVER////////////
 	private void send(Message message) {
 		try {
 			System.out.println("Sending: " + this.mapper.writeValueAsString(message));
@@ -192,40 +212,8 @@ public class GameClientController {
 			throw new RuntimeException(e);
 		}
 	}
-
-	private void outputList(final List<?> actions) {
-		Objects.requireNonNull(actions, "actions");
-		
-		Pane choiceBoxPane = new Pane();
-		choiceBoxPane.setPrefHeight(367);
-		choiceBoxPane.setPrefWidth(550);
-		choiceBoxPane.setTranslateX(326);
-		choiceBoxPane.setTranslateY(150);
-		choiceBoxPane.setId("choiceBoxPane");
-		
-		int numChoices = actions.size();
-		double paneHeight = choiceBoxPane.getPrefHeight();
-		double paneWidth = choiceBoxPane.getPrefWidth();
-		double choiceBoxWidth = paneWidth / numChoices;
-		
-		
-		
-		//print("------------------");
-		ListIterator<?> it = actions.listIterator();	
-		
-		int actionIndex = 0;
-		while (it.hasNext()) {
-			//print(it.nextIndex() + ": " + it.next());
-			Rectangle choiceBox = new Rectangle(choiceBoxWidth, paneHeight);
-			choiceBoxPane.getChildren().add(choiceBox);
-			
-			actionIndex++;
-		}
-		
-		Platform.runLater(() -> rootPane.getChildren().add(choiceBoxPane));
-	}
+	
 }
-
 
 //this is the syntax for multiple things inside a lambda
 /*
@@ -233,25 +221,4 @@ Platform.runLater(() -> {
 	serverMessage.setText(((WaitMessage)message).getMessage());
 });
 */
-/*
-Yes, right...
-the thing is that as this is multi-threaded, the listen method plays a big part here
-The playLoop only asks the console for input about what to do next
-I don't think you need the while-loop in the playLoop method. A GUI doesn't need that kind of loop.
-  
-The input should come from the GUI, not from a loop
-And then in the listen method, you'll have to filter out interesting messages and handle them (i.e. make the GUI show something)
-
-
-The while loop in listen holds the key
-Currently, its most useful feature is this line: System.out.println("iterator: " + mess);
-i.e. it prints all the messages it receives from the server to System.out
-i.e. user experience = horrible
-
-if you add some more mess instanceof checks in the while loop, for ZoneMessage, CardMessage and some others, and paint stuff in the GUI when your 
-receive those messages, then you will probably have better progress.
-Don't focus on parsing the text, Jackson takes care of that. All messages gets instantiated as a **Message class
-
-
-In cardshifter-api/com.cardshifter.server.outgoing you can see the possible messages that the server can send to the clients
-*/
+//In cardshifter-api/com.cardshifter.server.outgoing you can see the possible messages that the server can send to the clients
