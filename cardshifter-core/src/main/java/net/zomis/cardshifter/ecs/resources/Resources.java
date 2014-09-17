4807d15892cf63f9bf8862660ed596cb5f1559f9
Simon Forsberg <zomis2k@hotmail.com>
2014-09-17 17:23:27 +0200

    Added a function to get all resource data by `name: value`

--- cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/resources/Resources.java
+++ cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/resources/Resources.java
@@ -1,5 +1,7 @@
 package net.zomis.cardshifter.ecs.resources;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 import net.zomis.cardshifter.ecs.base.Entity;
@@ -8,6 +10,12 @@ public class Resources {
 
 	public static void processResources(Entity card, Consumer<ECSResourceData> consumer) {
 		card.getComponent(ECSResourceMap.class).getResources().forEach(consumer);
+	}
+
+	public static Map<String, Integer> map(Entity playerFor) {
+		HashMap<String, Integer> result = new HashMap<>();
+		processResources(playerFor, data -> result.put(data.getResource().toString(), data.get()));
+		return result;
 	}
 
 }
