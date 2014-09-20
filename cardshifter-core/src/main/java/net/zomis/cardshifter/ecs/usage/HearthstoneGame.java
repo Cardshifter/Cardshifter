39f5b43654a2ec205fac0ee6af9285c579e99be2
Simon Forsberg <zomis2k@hotmail.com>
2014-09-20 23:46:25 +0200

    Added work-in-progress Hearthstone game

---
+++ cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/usage/HearthstoneGame.java
@@ -0,0 +1,14 @@
+package net.zomis.cardshifter.ecs.usage;
+
+import net.zomis.cardshifter.ecs.base.ECSGame;
+
+public class HearthstoneGame {
+
+	
+	public ECSGame createGame() {
+		ECSGame game = new ECSGame();
+		
+		return game;
+	}
+	
+}
