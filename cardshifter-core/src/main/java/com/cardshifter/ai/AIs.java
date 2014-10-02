05bcb584d55ab796cbbf53f2de4520753c41f980
Simon Forsberg <zomis2k@hotmail.com>
2014-10-02 16:30:42 +0200

    Added some functionality for AI Medium

--- cardshifter-core/src/main/java/com/cardshifter/ai/AIs.java
+++ cardshifter-core/src/main/java/com/cardshifter/ai/AIs.java
@@ -2,9 +2,13 @@ package com.cardshifter.ai;
 
 import net.zomis.aiscores.ScoreConfigFactory;
 import net.zomis.aiscores.scorers.PredicateScorer;
+import net.zomis.aiscores.scorers.SimpleScorer;
 import net.zomis.cardshifter.ecs.actions.ECSAction;
 import net.zomis.cardshifter.ecs.base.Entity;
 import net.zomis.cardshifter.ecs.usage.PhrancisGame;
+
+import com.cardshifter.ai.phrancis.AttackAnalyze;
+import com.cardshifter.ai.phrancis.ScrapAnalyze;
 
 public class AIs {
 	
@@ -19,7 +23,11 @@ public class AIs {
 	}
 
 	public static ScoreConfigFactory<Entity, ECSAction> medium() {
-		return idiot();
+		ScoreConfigFactory<Entity, ECSAction> config = new ScoreConfigFactory<>();
+		config.withScorer(new PredicateScorer<>(action -> action.getName().equals(PhrancisGame.PLAY_ACTION)), 10);
+		config.withScorer(new SimpleScorer<>(ScrapAnalyze::scrapScore));
+		config.withScorer(new SimpleScorer<>(AttackAnalyze::attackScore));
+		return config;
 	}
 
 }
