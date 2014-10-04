414c538ed75cbad2fa8918510ba3ba43166a81b1
Janos Gyerik <janos@kronos>
2014-10-04 22:48:09 +0200

    improved ActionTest.testPerformAllowedAction and removed side-effecty things

--- cardshifter-core/src/test/java/net/zomis/cardshifter/ecs/ActionTest.java
+++ cardshifter-core/src/test/java/net/zomis/cardshifter/ecs/ActionTest.java
@@ -1,20 +1,20 @@
 package net.zomis.cardshifter.ecs;
 
-import static org.junit.Assert.*;
-
-import java.util.Collections;
-
-import net.zomis.cardshifter.ecs.actions.ActionAllowedCheckEvent;
-import net.zomis.cardshifter.ecs.actions.ActionComponent;
-import net.zomis.cardshifter.ecs.actions.ActionPerformEvent;
-import net.zomis.cardshifter.ecs.actions.ECSAction;
-import net.zomis.cardshifter.ecs.actions.SpecificActionSystem;
+import net.zomis.cardshifter.ecs.actions.*;
 import net.zomis.cardshifter.ecs.base.ComponentRetriever;
 import net.zomis.cardshifter.ecs.base.ECSGame;
 import net.zomis.cardshifter.ecs.base.Entity;
-
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class ActionTest {
 
@@ -22,25 +22,28 @@ public class ActionTest {
 	private Entity entity;
 	private ComponentRetriever<ActionComponent> retriever = ComponentRetriever.retreiverFor(ActionComponent.class);
 	private int used;
-	
+
 	@Before
 	public void das() {
 		game = new ECSGame();
 		entity = game.newEntity().addComponent(new ActionComponent());
 	}
-	
+
 	@Test
-	public void test() {
+	public void testPerformAllowedAction() {
 		game.startGame();
 		ActionComponent actions = retriever.get(entity);
 		assertEquals(Collections.<String>emptySet(), actions.getActions());
-		
-		actions.addAction(new ECSAction(entity, "Use", action -> true, action -> this.used++));
-		assertEquals(0, used);
-		actions.getAction("Use").copy().perform(entity);
-		assertEquals(1, used);
+
+		@SuppressWarnings("unchecked")
+		Consumer<ECSAction> perform = (Consumer<ECSAction>) mock(Consumer.class);
+
+		String name = "Use";
+		actions.addAction(new ECSAction(entity, name, action -> true, perform));
+		actions.getAction(name).copy().perform(entity);
+		verify(perform).accept(Mockito.any(ECSAction.class));
 	}
-	
+
 	@Test
 	public void deniedActionWithSystem() {
 		game.addSystem(new SpecificActionSystem("Use") {
@@ -48,18 +51,20 @@ public class ActionTest {
 			protected void isAllowed(ActionAllowedCheckEvent event) {
 				event.setAllowed(false);
 			}
+
 			@Override
-			protected void onPerform(ActionPerformEvent event) { }
+			protected void onPerform(ActionPerformEvent event) {
+			}
 		});
 		game.startGame();
-		
+
 		ActionComponent actions = retriever.get(entity);
 		assertEquals(Collections.<String>emptySet(), actions.getActions());
-		
+
 		actions.addAction(new ECSAction(entity, "Use", action -> true, action -> this.used++));
 		assertEquals(0, used);
 		assertFalse(actions.getAction("Use").isAllowed(entity));
 		assertEquals(0, used);
 	}
-	
+
 }
