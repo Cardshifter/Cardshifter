e4371e7a286cbd6cd8b11dc8da0e4dc323eec42d
Simon Forsberg <zomis2k@hotmail.com>
2014-10-05 18:30:07 +0200

    Added a system to draw a card when a creature dies

---
+++ cardshifter-core/src/main/java/net/zomis/cardshifter/ecs/usage/OnCreatureDiesDrawCard.java
@@ -0,0 +1,28 @@
+package net.zomis.cardshifter.ecs.usage;
+
+import net.zomis.cardshifter.ecs.base.ECSGame;
+import net.zomis.cardshifter.ecs.base.ECSSystem;
+import net.zomis.cardshifter.ecs.base.Entity;
+import net.zomis.cardshifter.ecs.base.EntityRemoveEvent;
+import net.zomis.cardshifter.ecs.cards.BattlefieldComponent;
+import net.zomis.cardshifter.ecs.cards.CardComponent;
+import net.zomis.cardshifter.ecs.cards.Cards;
+import net.zomis.cardshifter.ecs.cards.DrawStartCards;
+
+public class OnCreatureDiesDrawCard implements ECSSystem {
+
+	@Override
+	public void startGame(ECSGame game) {
+		game.getEvents().registerHandlerBefore(this, EntityRemoveEvent.class, this::removed);
+	}
+	
+	private void removed(EntityRemoveEvent event) {
+		if (event.getEntity().hasComponent(CardComponent.class)) {
+			if (Cards.isOnZone(event.getEntity(), BattlefieldComponent.class)) {
+				Entity owner = event.getEntity().getComponent(CardComponent.class).getOwner();
+				DrawStartCards.drawCard(owner);
+			}
+		}
+	}
+
+}
