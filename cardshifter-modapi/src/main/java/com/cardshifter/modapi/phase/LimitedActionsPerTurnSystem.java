63ef74bfa5e3a4bde4a3cba06549ce44ebba6f2c
Simon Forsberg <zomis2k@hotmail.com>
2014-10-08 11:56:06 +0200

    Added `LimitedActionsPerTurnSystem`

---
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/phase/LimitedActionsPerTurnSystem.java
@@ -0,0 +1,43 @@
+package com.cardshifter.modapi.phase;
+
+import com.cardshifter.modapi.actions.ActionPerformEvent;
+import com.cardshifter.modapi.actions.Actions;
+import com.cardshifter.modapi.actions.ECSAction;
+import com.cardshifter.modapi.base.ECSGame;
+import com.cardshifter.modapi.base.ECSSystem;
+
+/**
+ * <p>Functionality for automatically ending phase once x cards has been played that turn. (ActionZone cards not included)</p>
+ * <p>Listens for {@link CardPlayedEvent} and {@link PhaseChangeEvent}</p>
+ */
+public class LimitedActionsPerTurnSystem implements ECSSystem {
+
+	private int cardsPlayedThisTurn;
+	private final int limit;
+	private final String actionName;
+	
+	public LimitedActionsPerTurnSystem(int limit, String actionName) {
+		this.limit = limit;
+		this.actionName = actionName;
+	}
+	
+	@Override
+	public void startGame(ECSGame game) {
+		game.getEvents().registerHandlerAfter(this, ActionPerformEvent.class, this::onCardPlayed);
+		game.getEvents().registerHandlerAfter(this, PhaseChangeEvent.class, this::onNewTurn);
+	}
+	
+	private void onNewTurn(PhaseChangeEvent event) {
+		// TODO: This is technically not turn-dependent, only phase-dependent. One *turn* can consist of many *phases*
+		this.cardsPlayedThisTurn = 0;
+	}
+	
+	private void onCardPlayed(ActionPerformEvent event) {
+		this.cardsPlayedThisTurn++;
+		if (this.cardsPlayedThisTurn >= limit) {
+			ECSAction action = Actions.getAction(event.getPerformer(), actionName);
+			action.perform(event.getPerformer());
+		}
+	}
+
+}
