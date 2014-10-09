0284af147ea279153ec594b95635d8701ca0ef93
Simon Forsberg <zomis2k@hotmail.com>
2014-10-09 12:12:26 +0200

    Added `declareConfiguration` default method to ECSMod

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/base/ECSMod.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/base/ECSMod.java
@@ -1,5 +1,20 @@
 package com.cardshifter.modapi.base;
 
 public interface ECSMod {
+	/**
+	 * This method is called before asking each individual player to configure their options.
+	 * 
+	 * If functionality such as Deck-building is desired, this method needs to setup `PlayerComponents` to entities, and add a `ConfigComponent` to those entities
+	 *   
+	 * @param game
+	 */
+	default void declareConfiguration(ECSGame game) {
+		
+	}
+	
+	/**
+	 * This is the final method to be called in the game setup process. This should setup the game to be playable.
+	 * @param game The game to setup
+	 */
 	void setupGame(ECSGame game);
 }
