5e1d26e07963faeb208918b7f0603078637f0d6d
Simon Forsberg <zomis2k@hotmail.com>
2014-10-21 12:38:17 +0200

    Fixed inf-loop in ServerConnectionTest

--- cardshifter-server/src/main/java/com/cardshifter/server/clients/ClientSocketHandler.java
+++ cardshifter-server/src/main/java/com/cardshifter/server/clients/ClientSocketHandler.java
@@ -51,7 +51,7 @@ public class ClientSocketHandler extends ClientIO implements Runnable {
 		} catch (IOException e) {
 			String error = "Error occured when sending message " + message;
 			logger.fatal(error, e);
-			this.close();
+//			this.disconnected(); // Possibly mark client as disconnected here
 		}
 	}
 
@@ -89,18 +89,6 @@ public class ClientSocketHandler extends ClientIO implements Runnable {
 	@Override
 	public void close() {
 		this.disconnected();
-//		try {
-//			logger.info(this + " Closing input stream");
-//			in.close();
-//		} catch (IOException e1) {
-//			logger.warn("Error closing input stream", e1);
-//		}
-//		try {
-//			logger.info(this + " Closing output stream");
-//			out.close();
-//		} catch (IOException e1) {
-//			logger.warn("Error closing output stream", e1);
-//		}
 		try {
 			logger.info(this + " Closing socket");
 			socket.close();
