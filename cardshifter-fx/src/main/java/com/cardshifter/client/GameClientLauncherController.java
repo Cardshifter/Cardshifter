8f05b7f8335f55b5684be5eed5c45a4ddfd68a43
Simon Forsberg <zomis2k@hotmail.com>
2014-10-23 23:22:13 +0200

    Changed local game client to use the same GUI as the online client

--- cardshifter-fx/src/main/java/com/cardshifter/client/GameClientLauncherController.java
+++ cardshifter-fx/src/main/java/com/cardshifter/client/GameClientLauncherController.java
@@ -1,12 +1,16 @@
 package com.cardshifter.client;
 
-import com.cardshifter.client.buttons.AIChoiceButton;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.prefs.Preferences;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,11 +24,26 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import net.zomis.cardshifter.ecs.usage.DeckConfig;
+import net.zomis.cardshifter.ecs.usage.PhrancisGame;
 
 import com.cardshifter.ai.AIs;
+import com.cardshifter.ai.FakeAIClientTCG;
 import com.cardshifter.ai.ScoringAI;
-import com.cardshifter.fx.FXMLGameController;
+import com.cardshifter.api.ClientIO;
+import com.cardshifter.api.ClientServerInterface;
+import com.cardshifter.api.both.PlayerConfigMessage;
+import com.cardshifter.api.incoming.RequestTargetsMessage;
+import com.cardshifter.api.incoming.UseAbilityMessage;
+import com.cardshifter.api.messages.Message;
+import com.cardshifter.api.outgoing.NewGameMessage;
+import com.cardshifter.client.buttons.AIChoiceButton;
+import com.cardshifter.core.game.FakeClient;
+import com.cardshifter.core.game.TCGGame;
 import com.cardshifter.modapi.ai.AIComponent;
+import com.cardshifter.modapi.base.ECSMod;
+import com.cardshifter.modapi.base.Entity;
+import com.cardshifter.modapi.base.PlayerComponent;
 
 public final class GameClientLauncherController implements Initializable {
 	
@@ -40,6 +59,7 @@ public final class GameClientLauncherController implements Initializable {
 	private final Map<String, AIComponent> aiChoices = new HashMap<>();
 	private AIComponent aiChoice;
 	private final Preferences settings = Preferences.userNodeForPackage(GameClientLauncherController.class);
+	private ClientIO human;
 	
 	private static final String CONF_NAME = "name";
 
@@ -116,32 +136,118 @@ public final class GameClientLauncherController implements Initializable {
 	}
 	
 	private void localGameStart(ActionEvent event) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("SinglePlayerDocument.fxml"));
-			Parent root = loader.load();
-			
-			FXMLGameController controller = loader.<FXMLGameController>getController();
-			
-			if (this.aiChoice != null) {
-				controller.acceptAIChoice(this.aiChoice);
-				controller.initializeGame();
-			
-				this.closeWithSuccess();
-		
-				Scene scene = new Scene(root);
-				Stage stage = new Stage();
-		
-				stage.setScene(scene);
-				stage.show();
-			} else {
-				System.out.println("AI not selected!");
+		ECSMod mod = new PhrancisGame();
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+		TCGGame game = new TCGGame(() -> executor, 1, mod);
+		ClientServerInterface singlePlayerHandler = new ClientServerInterface() {
+			@Override
+			public void performIncoming(Message message, ClientIO clientIO) {
+				if (message instanceof UseAbilityMessage) {
+					UseAbilityMessage msg = (UseAbilityMessage) message;
+					game.handleMove(msg, clientIO);
+				}
+				if (message instanceof RequestTargetsMessage) {
+					RequestTargetsMessage msg = (RequestTargetsMessage) message;
+					game.informAboutTargets(msg, clientIO);
+				}
+				if (message instanceof PlayerConfigMessage) {
+					PlayerConfigMessage msg = (PlayerConfigMessage) message;
+					game.incomingPlayerConfig(msg, clientIO);
+				}
 			}
-
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+			
+			@Override
+			public void onDisconnected(ClientIO clientIO) {
+			}
+			
+			@Override
+			public int newClientId() {
+				return 0;
+			}
+			
+			@Override
+			public void handleMessage(ClientIO clientIO, String message) {
+				throw new UnsupportedOperationException();
+			}
+		};
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("ClientDocument.fxml"));
+			Parent root = (Parent)loader.load();
+			GameClientController controller = loader.<GameClientController>getController();
+			human = new FakeClient(singlePlayerHandler, msg -> handleMessage(msg, controller));
+			ClientIO ai = new FakeAIClientTCG(singlePlayerHandler, this.aiChoice.getAI());
+			game.start(Arrays.asList(human, ai));
+			Entity humanPlayer = game.playerFor(human);
+			PlayerComponent playerInfo = humanPlayer.getComponent(PlayerComponent.class);
+			controller.acceptConnectionSettings(new NewGameMessage(0, playerInfo.getIndex()), e -> human.sentToServer(e));
+			
+			Scene scene = new Scene(root);
+			Stage gameStage = new Stage();
+			gameStage.setScene(scene);
+			gameStage.setOnCloseRequest(windowEvent -> {});
+			gameStage.show();
 		}
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 	}
 	
+	private void handleMessage(Message msg, GameClientController controller) {
+		if (msg instanceof PlayerConfigMessage) {
+			PlayerConfigMessage configMessage = (PlayerConfigMessage) msg;
+			
+			Map<String, Object> configs = configMessage.getConfigs();
+			for (Entry<String, Object> entry : configs.entrySet()) {
+				Object value = entry.getValue();
+				if (value instanceof DeckConfig) {
+					DeckConfig deckConfig = (DeckConfig) value;
+					this.showDeckBuilderWindow(configMessage, deckConfig, true);
+				}
+			}		
+			return;
+		}
+		Platform.runLater(() -> controller.processMessageFromServer(msg));
+	}
+
+	private void showDeckBuilderWindow(PlayerConfigMessage conf, DeckConfig deckConfig, boolean startingGame) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("DeckBuilderDocument.fxml"));
+			Parent root = (Parent)loader.load();
+			DeckBuilderWindow controller = loader.<DeckBuilderWindow>getController();
+			
+			controller.acceptDeckConfig(deckConfig, cnf -> human.sentToServer(incorporateConfig(conf, deckConfig, cnf)));
+			controller.configureWindow();
+			
+			if (!startingGame) {
+				controller.disableGameStart();
+			}
+			
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+//			stage.setOnCloseRequest(windowEvent -> this.closeDeckBuilderWindow());
+			stage.show();
+		}
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+	}
+	
+	private Message incorporateConfig(PlayerConfigMessage conf, DeckConfig oldConfig, DeckConfig newConfig) {
+		Map<String, Object> configs = conf.getConfigs();
+		
+		for (Entry<String, Object> entry : configs.entrySet()) {
+			Object value = entry.getValue();
+			if (value instanceof DeckConfig) {
+				DeckConfig config = (DeckConfig) value;
+				newConfig.getChosen().forEach((id, count) -> config.setChosen(id, count));
+			}
+		}
+		
+		return new PlayerConfigMessage(conf.getGameId(), configs);
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		this.connectButton.setOnAction(this::buttonClick);
