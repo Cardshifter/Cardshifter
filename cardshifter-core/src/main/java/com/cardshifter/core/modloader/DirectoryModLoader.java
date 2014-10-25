f733df7ee55f40f12c4b6a04b3eb520616b3d8db
skiwi <frank_skiwi@hotmail.com>
2014-10-25 12:42:55 +0200

    Added getAvailableMods() implementation to the DirectoryModLoader.

--- cardshifter-core/src/main/java/com/cardshifter/core/modloader/DirectoryModLoader.java
+++ cardshifter-core/src/main/java/com/cardshifter/core/modloader/DirectoryModLoader.java
@@ -1,6 +1,8 @@
 
 package com.cardshifter.core.modloader;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -8,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -72,6 +75,13 @@ public final class DirectoryModLoader implements ModLoader {
 
 	@Override
 	public List<String> getAvailableMods() {
-		throw new UnsupportedOperationException();
+		try {
+			return Files.list(modsDirectory)
+				.filter(Files::isDirectory)
+				.map(path -> path.getFileName().toString())
+				.collect(Collectors.toList());
+		} catch (IOException ex) {
+			throw new UncheckedIOException(ex);
+		}
 	}
 }
