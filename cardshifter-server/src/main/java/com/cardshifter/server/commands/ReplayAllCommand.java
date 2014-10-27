0bc42eae3a38e63152e38b2252355aa5c44b50f1
Simon Forsberg <zomis2k@hotmail.com>
2014-10-27 01:34:48 +0100

    Added `/allreplays` command to verify existing replays

---
+++ cardshifter-server/src/main/java/com/cardshifter/server/commands/ReplayAllCommand.java
@@ -0,0 +1,84 @@
+package com.cardshifter.server.commands;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import net.zomis.cardshifter.ecs.usage.CardshifterIO;
+
+import org.apache.log4j.LogManager;
+
+import com.cardshifter.core.game.FakeClient;
+import com.cardshifter.core.game.TCGGame;
+import com.cardshifter.core.replays.ReplayPlaybackSystem;
+import com.cardshifter.core.replays.ReplayRecordSystem;
+import com.cardshifter.modapi.base.ECSGameState;
+import com.cardshifter.server.commands.ReplayAllCommand.ReplayAllParameters;
+import com.cardshifter.server.model.CommandHandler.CommandHandle;
+import com.cardshifter.server.model.Server;
+
+public class ReplayAllCommand implements CommandHandle<ReplayAllParameters> {
+
+	public static class ReplayAllParameters {
+	}
+	
+	@Override
+	public void handle(CommandContext command, ReplayAllParameters parameters) {
+		File dir = new File(".");
+		for (File file : dir.listFiles()) {
+			if (!file.getName().startsWith("replay-")) {
+				continue;
+			}
+			if (!file.getName().endsWith(".json")) {
+				continue;
+			}
+			
+			String replayMod = checkFile(command, file);
+			command.sendChatResponse(file + " detected as " + replayMod);
+		}
+	}
+
+	private String checkFile(CommandContext command, File file) {
+		for (String mod : command.getServer().getGameFactories().keySet()) {
+			try {
+				if (checkReplay(command.getServer(), file, mod)) {
+					return mod;
+				}
+			}
+			catch (RuntimeException ex) {
+				command.sendChatResponse("Error checking replay " + file + " with mod " + mod + ": " + ex.getMessage());
+				LogManager.getLogger(getClass()).error("Error checking replay " + file + " with mod " + mod, ex);
+			}
+		}
+		return null;
+	}
+
+	private boolean checkReplay(Server server, File file, String mod) {
+		TCGGame game = (TCGGame) server.createGame(mod);
+		ReplayRecordSystem replay;
+		try {
+			replay = CardshifterIO.mapper().readValue(file, ReplayRecordSystem.class);
+		} catch (IOException e1) {
+			throw new RuntimeException("Error loading replay: " + e1.getMessage(), e1);
+		}
+		
+		ReplayPlaybackSystem playback = new ReplayPlaybackSystem(game.getGameModel(), replay);
+		game.getGameModel().addSystem(playback);
+		FakeClient fake1 = new FakeClient(server, e -> {});
+		FakeClient fake2 = new FakeClient(server, e -> {});
+		game.start(Arrays.asList(fake1, fake2));
+		System.out.println("Game state is " + game.getState());
+		if (game.getState() == ECSGameState.NOT_STARTED) {
+			System.out.println("Loading configs from saved data");
+			playback.setPlayerConfigs(game.getGameModel());
+			game.checkStartGame();
+		}
+	
+		while (!playback.isReplayFinished()) {
+			playback.nextStep();
+		}
+		
+		return game.isGameOver();
+	}
+
+}
--- cardshifter-server/src/main/java/com/cardshifter/server/model/MainServer.java
+++ cardshifter-server/src/main/java/com/cardshifter/server/model/MainServer.java
@@ -22,10 +22,12 @@ import com.cardshifter.server.commands.AICommand;
 import com.cardshifter.server.commands.AICommand.AICommandParameters;
 import com.cardshifter.server.commands.CommandContext;
 import com.cardshifter.server.commands.EntityCommand;
-import com.cardshifter.server.commands.ReplayCommand;
 import com.cardshifter.server.commands.EntityCommand.EntityInspectParameters;
 import com.cardshifter.server.commands.HelpCommand;
 import com.cardshifter.server.commands.HelpCommand.HelpParameters;
+import com.cardshifter.server.commands.ReplayAllCommand;
+import com.cardshifter.server.commands.ReplayAllCommand.ReplayAllParameters;
+import com.cardshifter.server.commands.ReplayCommand;
 import com.cardshifter.server.commands.ReplayCommand.ReplayParameters;
 import com.cardshifter.server.utils.export.DataExporter;
 
@@ -103,6 +105,7 @@ public class MainServer {
 		commandHandler.addHandler("ent", () -> new EntityInspectParameters(), new EntityCommand());
 		commandHandler.addHandler("threads", cmd -> showAllStackTraces(server, System.out::println));
 		commandHandler.addHandler("replay", () -> new ReplayParameters(), new ReplayCommand());
+		commandHandler.addHandler("allreplays", () -> new ReplayAllParameters(), new ReplayAllCommand());
 	}
 	
 	/**
