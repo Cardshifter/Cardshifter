25ec221110d5ff0d4718c8014c79ed775ca80292
skiwi <frank_skiwi@hotmail.com>
2014-10-28 21:30:34 +0100

    Added toString() methods to CardImageComponent and CardTypeComponent.

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/cards/CardImageComponent.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/cards/CardImageComponent.java
@@ -19,4 +19,9 @@ public class CardImageComponent extends Component {
 	public String getCardImage() {
 		return cardImage;
 	}
+	
+	@Override
+	public String toString() {
+		return "CardImage: " + cardImage;
+	}
 }
--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/cards/CardTypeComponent.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/cards/CardTypeComponent.java
@@ -19,4 +19,9 @@ public class CardTypeComponent extends Component {
 	public String getCardType() {
 		return cardType;
 	}
+	
+	@Override
+	public String toString() {
+		return "CardType: " + cardType;
+	}
 }
