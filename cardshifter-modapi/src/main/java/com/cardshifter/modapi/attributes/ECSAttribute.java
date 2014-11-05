8582c727a345c37723a2d3b6751b816893c0ec8b
skiwi <frank_skiwi@hotmail.com>
2014-11-05 14:19:04 +0100

    Added javadoc to the getFor default method of ECSResource and ECSAttribute.

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/attributes/ECSAttribute.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/attributes/ECSAttribute.java
@@ -6,6 +6,12 @@ import com.cardshifter.modapi.base.Entity;
  * Interface for Attribute types
  */
 public interface ECSAttribute {
+	/**
+	 * Returns the value of this attribute associated with the given entity.
+	 * 
+	 * @param entity	The entity for which the attribute value is to be retrieved
+	 * @return	The attribute value
+	 */
 	default String getFor(final Entity entity) {
 		return AttributeRetriever.forAttribute(this).getFor(entity);
 	}
--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/resources/ECSResource.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/resources/ECSResource.java
@@ -6,6 +6,12 @@ import com.cardshifter.modapi.base.Entity;
  * Interface for Resource types
  */
 public interface ECSResource {
+	/**
+	 * Returns the value of this resource associated with the given entity.
+	 * 
+	 * @param entity	The entity for which the resource value is to be retrieved
+	 * @return	The resource value
+	 */
 	default int getFor(final Entity entity) {
 		return ResourceRetriever.forResource(this).getFor(entity);
 	}
