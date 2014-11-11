e09bdd5c102f30dedec19aa5fd3f0e7a14aca151
Simon Forsberg <zomis2k@hotmail.com>
2014-11-11 20:32:40 +0100

    added simple GDX project inside the 'gdx' directory

---
+++ gdx/desktop/src/com/cardshifter/gdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cardshifter.gdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cardshifter.gdx.CardshifterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CardshifterGame(), config);
+	}
+}
