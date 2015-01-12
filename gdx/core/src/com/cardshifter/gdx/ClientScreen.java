04f2f1b9e031508f8abecc755fa4a5e53fc9e78a
Simon Forsberg <zomis2k@hotmail.com>
2015-01-12 23:34:31 +0100

    now showing the available mods when logging in

--- gdx/core/src/com/cardshifter/gdx/ClientScreen.java
+++ gdx/core/src/com/cardshifter/gdx/ClientScreen.java
@@ -2,8 +2,12 @@ package com.cardshifter.gdx;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.cardshifter.api.incoming.LoginMessage;
 import com.cardshifter.api.messages.Message;
+import com.cardshifter.api.outgoing.AvailableModsMessage;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,11 +18,27 @@ import java.util.Map;
 public class ClientScreen implements Screen, CardshifterMessageHandler {
     private final CardshifterClient client;
     private final Map<Class<? extends Message>, SpecificHandler<?>> handlerMap = new HashMap<Class<? extends Message>, SpecificHandler<?>>();
+    private final Table table;
+    private final HorizontalGroup mods;
     private final CardshifterGame game;
 
     public ClientScreen(final CardshifterGame game, String host, int port) {
         this.game = game;
         client = new CardshifterClient(host, port, this);
+        table = new Table(game.skin);
+        table.setFillParent(true);
+        mods = new HorizontalGroup();
+        table.add(mods).bottom().expand().fill();
+        table.setDebug(true, true);
+        handlerMap.put(AvailableModsMessage.class, new SpecificHandler<AvailableModsMessage>() {
+            @Override
+            public void handle(AvailableModsMessage message) {
+                Gdx.app.log("handle", "handle " + message);
+                for (String mod : message.getMods()) {
+                    mods.addActor(new TextButton(mod, game.skin));
+                }
+            }
+        });
         Gdx.app.postRunnable(new Runnable() {
             @Override
             public void run() {
@@ -43,12 +63,12 @@ public class ClientScreen implements Screen, CardshifterMessageHandler {
 
     @Override
     public void show() {
-
+        game.stage.addActor(table);
     }
 
     @Override
     public void hide() {
-
+        table.remove();
     }
 
     @Override
