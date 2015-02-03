e59e1fb1c7acfc24860a2c53a7747c1a54bf2023
Simon Forsberg <zomis2k@hotmail.com>
2015-02-03 12:29:59 +0100

    adding cards to DefaultZoneView

--- gdx/core/src/com/cardshifter/gdx/DefaultZoneView.java
+++ gdx/core/src/com/cardshifter/gdx/DefaultZoneView.java
@@ -5,19 +5,23 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.cardshifter.api.outgoing.CardInfoMessage;
 import com.cardshifter.api.outgoing.ZoneMessage;
+import com.cardshifter.gdx.ui.CardView;
 import com.cardshifter.gdx.ui.ZoneView;
 
 public class DefaultZoneView extends ZoneView {
 
     private final Table table;
+    private final CardshifterGame game;
 
     public DefaultZoneView(CardshifterGame game, ZoneMessage message) {
         this.table = new Table(game.skin);
+        this.game = game;
     }
 
     @Override
     public void addCard(CardInfoMessage message) {
-
+        CardView view = new CardView(this.game, message);
+        table.add(view.getTable());
     }
 
     @Override
