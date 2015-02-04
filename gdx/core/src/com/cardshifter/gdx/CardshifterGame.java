24ab2b7a9c7f132c6ddf24e4af47a99ca3d85c18
Simon Forsberg <zomis2k@hotmail.com>
2015-02-04 13:21:50 +0100

    switched background color to black

--- gdx/core/src/com/cardshifter/gdx/CardshifterGame.java
+++ gdx/core/src/com/cardshifter/gdx/CardshifterGame.java
@@ -49,7 +49,7 @@ public class CardshifterGame extends Game {
 
     @Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
