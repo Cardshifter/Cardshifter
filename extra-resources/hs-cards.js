38f9774b62fd9c579b2b32e72acec601b631d6b8
skiwi <frank_skiwi@hotmail.com>
2015-03-14 16:09:43 +0100

    Added missing cards file.

---
+++ extra-resources/hs-cards.js
@@ -0,0 +1,11 @@
+function getCards() {
+    return [
+        {
+            name: "Murloc Raider",
+            attack: 2,
+            hitpoints: 1,
+            cost: 1,
+            race: "Murloc"
+        }
+    ];
+}
