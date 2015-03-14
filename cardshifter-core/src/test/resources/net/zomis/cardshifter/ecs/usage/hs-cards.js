71a10ec7029f8c8af9702bc57ae390c2b2b2bcf4
skiwi <frank_skiwi@hotmail.com>
2015-03-14 18:59:29 +0100

    Improved attack_available and sickness property check.

--- cardshifter-core/src/test/resources/net/zomis/cardshifter/ecs/usage/hs-cards.js
+++ cardshifter-core/src/test/resources/net/zomis/cardshifter/ecs/usage/hs-cards.js
@@ -16,12 +16,48 @@ var ECSAction = Java.type("com.cardshifter.modapi.actions.ECSAction");
 var HearthstoneGame = Java.type("net.zomis.cardshifter.ecs.usage.HearthstoneGame");
 
 function mapCard(card) {
+    //use regex to match property on on*PhaseEnd
+    var functions = [];
+    var saveObject = {};
+    for (var property in card) {
+        if (!card.hasOwnProperty(property)) {
+            continue;
+        }
+        if (typeof card[property] !== "function") {
+            continue;
+        }
+
+
+        var matches = property.match(/^on(.*)PhaseEnd$/);
+        if (matches) {
+            var phaseName = matches[1];
+            saveObject[property] = card[property];
+            delete card[property];
+            functions.push((function (phaseName, property) {
+                return function (game, event) {
+                    if (event.getOldPhase().getName() === phaseName) {
+                        saveObject[property](game, event);
+                    }
+                }
+            })(phaseName, property));
+        }
+    }
+
+    if (card.onPhaseEnd) {
+        functions.push(card.onPhaseEnd);
+    }
+    card.onPhaseEnd = function(game, event) {
+        for (var i = 0; i < functions.length; i++) {
+            functions[i](game, event);
+        }
+    }
+
     //setup if type is Minion
     if (card.type === "Minion") {
-        if (!card.attack_available) {
+        if (!card.hasOwnProperty("attack_available")) {
             card.attack_available = 1;
         }
-        if (!card.sickness) {
+        if (!card.hasOwnProperty("sickness")) {
             card.sickness = 1;
         }
 
