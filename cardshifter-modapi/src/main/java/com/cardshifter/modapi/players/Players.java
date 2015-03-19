720059d35c635490bbb0217d21db9e0bd72a20ca
Simon Forsberg <zomis2k@hotmail.com>
2015-03-19 19:01:10 +0100

    added Players.getPlayersInGame method

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/players/Players.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/players/Players.java
@@ -1,9 +1,6 @@
 package com.cardshifter.modapi.players;
 
-import com.cardshifter.modapi.base.ComponentRetriever;
-import com.cardshifter.modapi.base.Entity;
-import com.cardshifter.modapi.base.PlayerComponent;
-import com.cardshifter.modapi.base.Retrievers;
+import com.cardshifter.modapi.base.*;
 import com.cardshifter.modapi.cards.CardComponent;
 
 import java.util.*;
@@ -29,4 +26,11 @@ public class Players {
         Optional<Entity> result = players.stream().filter(pl -> player.get(pl).getIndex() == myIndex + 1).findFirst();
         return result.orElse(players.stream().filter(pl -> player.get(pl).getIndex() == 0).findFirst().get());
     }
+
+    public static List<Entity> getPlayersInGame(ECSGame game) {
+        List<Entity> players = game.findEntities(e -> e.hasComponent(PlayerComponent.class));
+        players.sort(Comparator.comparingInt(pl -> pl.getComponent(PlayerComponent.class).getIndex()));
+        return players;
+    }
+
 }
