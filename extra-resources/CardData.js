a74c88b83457ac6729162109f38645bac99266df
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-04-29 15:55:03 -0400

    Update and rename card-data.js to CardData.js
    Further modified/improved JSON data structure

--- extra-resources/CardData.js
+++ extra-resources/CardData.js
@@ -1,37 +1,49 @@
+/**
+ * Contains the library of cards available for a Cardshifter game in JSON format.
+ * Note: Please use the 'effects' wrapper only for properties that change the *behavior* of a card.
+ *   Keep regular/arbitrary attributes outside of effects to keep things organized.
+ * 
+ * @module CardData
+ */
+
+
 function loadCardLibrary() {
     var cardLibrary = {
         entities: [
             {
                 name: "Spareparts",
                 flavor: "Cobbled together from whatever was lying around at the time.",
-                values: {
-                    creature: "Mech",
-                    noAttack: true,
+                creature: true,
+                creatureType: "Mech",
+                manaCost: 0,
+                health: 1,
+                attack: 0,
+                scrap: 3,
+                effects: {
                     sickness: 0,
-                    manaCost: 0,
-                    health: 1,
-                    attack: 0,
-                    scrap: 3,
+                    noAttack: true
                 }
             },
             {
                 name: "Longshot",
                 flavor: "Eyes and reflexes augmented for maximum deadliness.",
-                values: {
-                    creature: "Bio",
-                    denyCounterAttack: 1,
-                    manaCost: 3,
-                    health: 1,
-                    attack: 3
-                },
+                creature: true,
+                creatureType: "Bio",
+                manaCost: 3,
+                health: 1,
+                attack: 3,
+                effects: {
+                    denyCounterAttack: 1
+                }
             },
             {
                 name: "Bionic Arms",
                 flavor: "These arms will give strength to even the most puny individual.",
-                values: {
+                enchantment: true,
+                scrapCost: 1,
+                effects: {
                     addAttack: 2,
                     addHealth: 0,
-                    scrapCost: 1,
                 },
             }
         ]
