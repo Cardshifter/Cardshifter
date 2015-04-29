607dcbbb7d22d6fdd29fd0ffde0c9b9b3a7e10f0
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-04-29 15:30:27 -0400

    Update card-data.js
    Tentative reformatting of JSON. Moved "apply" attributes to instead be values. More reformatting might happen after discussing some more with the team.

--- extra-resources/card-data.js
+++ extra-resources/card-data.js
@@ -5,40 +5,34 @@ function loadCardLibrary() {
                 name: "Spareparts",
                 flavor: "Cobbled together from whatever was lying around at the time.",
                 values: {
+                    creature: "Mech",
+                    noAttack: true,
                     sickness: 0,
+                    manaCost: 0,
+                    health: 1,
                     attack: 0,
                     scrap: 3,
-                    manaCost: 0
-                },
-                apply: [
-                    applyCreature("Mech"),
-                    applyHealth(1),
-                    applyNoAttack(true)
-                ]
+                }
             },
             {
                 name: "Longshot",
                 flavor: "Eyes and reflexes augmented for maximum deadliness.",
                 values: {
-                    DENY_COUNTERATTACK: 1,
+                    creature: "Bio",
+                    denyCounterAttack: 1,
                     manaCost: 3,
+                    health: 1,
                     attack: 3
                 },
-                apply: [
-                    applyCreature("Bio"),
-                    applyHealth(1)
-                ]
             },
             {
                 name: "Bionic Arms",
                 flavor: "These arms will give strength to even the most puny individual.",
                 values: {
-                    attack: 2,
+                    addAttack: 2,
+                    addHealth: 0,
                     scrapCost: 1,
                 },
-                apply: [
-                    applyHealth(0)
-                ]
             }
         ]
     };
@@ -73,5 +67,4 @@ var cardLibrary = loadCardLibrary();
 
 console.log(cardLibrary);
 console.log(cardLibrary.entities[0].values);
-console.log(cardLibrary.entities[0].apply);
 */
