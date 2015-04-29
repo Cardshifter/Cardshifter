6fe101c4482462d46807dc905d76146a248972aa
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-04-29 17:54:07 -0400

    Create CardDataLoader.js
    Related to #193

---
+++ extra-resources/CardDataLoader.js
@@ -0,0 +1,11 @@
+/**
+ * This module is for loading card attributes and resources to the Cardshifter game server
+ *   using ECSResourceMap and ECSAttributeMap. 
+ * Resources and attributes are obtained from CardData.js module
+ * 
+ * @module CardAttributeLoader
+ */
+ 
+function loadCardData(cardLibrary) {
+
+}
