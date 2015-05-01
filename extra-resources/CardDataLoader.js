58f8b15f1440cee5c9536290ef95278690045614
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-05-01 12:28:03 -0400

    Update CardDataLoader.js
    Remove duplicated game config code. See GameConfig.js file

--- extra-resources/CardDataLoader.js
+++ extra-resources/CardDataLoader.js
@@ -26,33 +26,6 @@ var ECSAttributeMap = Java.type("com.cardshifter.modapi.attributes.ECSAttributeM
 var baseMod = Java.type("net.zomis.cardshifter.ecs.usage.PhrancisGame");
 var Attributes = Java.type("com.cardshifter.modapi.attributes.Attributes");
 
-/**
- * Declare game configuration (CONCEPT)
- * @param {Object} game - Game configuration data
- */
-function declareConfiguration(game) {
-	var neutral = game.newEntity();
-	var zone = new ZoneComponent(neutral, "Cards");
-	neutral.addComponent(zone);
-	addCards(zone);
-	
-	/** Parameters related to DeckConfigFactory */
-	var maxCardsPerType = 3;
-	var minSize = 30;
-	var maxSize = 30;
-	
-	/**
-	 * Create playerComponent 0 & 1, i.e., Player1 & Player2
-	 * Config a deck for each player
-	 */
-	for (var i = 0; i < 2; i++) {
-		var entity = game.newEntity();
-		var playerComponent = new PlayerComponent(i, "Player" + (i+1));
-		entity.addComponent(playerComponent);
-		var config = DeckConfigFactory.create(minSize, maxSize, zone.getCards(), maxCardsPerType);
-		entity.addComponent(new ConfigComponent().addConfig("Deck", config));
-	}
-}
 
 /**
  * Load cards from JSON (CONCEPT)
