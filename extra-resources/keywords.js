04500d9b66c1701d21ed887f543cf532b96eceee
Simon Forsberg <zomis2k@hotmail.com>
2015-05-03 18:35:15 +0200

    added keywords: scrapCost, addAttack, addHealth

--- extra-resources/keywords.js
+++ extra-resources/keywords.js
@@ -85,6 +85,28 @@ keywords.cards.enchantment = function (entity, obj, value) {
 	entity.addComponent(actions);
 }
 
+keywords.cards.scrapCost = function (entity, obj, value) {
+    if (!obj.enchantment) {
+        throw new Error("expected enchantment");
+    }
+	com.cardshifter.modapi.resources.ECSResourceMap.createOrGetFor(entity).set(pgres.SCRAP_COST, value);
+}
+
+keywords.cards.addAttack = function (entity, obj, value) {
+    if (!obj.enchantment) {
+        throw new Error("expected enchantment");
+    }
+	com.cardshifter.modapi.resources.ECSResourceMap.createOrGetFor(entity).set(pgres.ATTACK, value);
+}
+
+keywords.cards.addHealth = function (entity, obj, value) {
+    if (!obj.enchantment) {
+        throw new Error("expected enchantment");
+    }
+	com.cardshifter.modapi.resources.ECSResourceMap.createOrGetFor(entity).set(pgres.HEALTH, value);
+	com.cardshifter.modapi.resources.ECSResourceMap.createOrGetFor(entity).set(pgres.MAX_HEALTH, value);
+}
+
 function applyEntity(game, card, entity, keyword) {
     print("applyEntity " + card + ": " + entity);
 
