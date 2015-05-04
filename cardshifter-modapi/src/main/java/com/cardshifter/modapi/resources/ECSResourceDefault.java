a4a5b58bd0daf69e9cb01c417146b88a6f9b97ae
Simon Forsberg <zomis2k@hotmail.com>
2015-05-04 17:18:18 +0200

    added retriever property to ECSResourceDefault, to avoid unnecessary object creation

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/resources/ECSResourceDefault.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/resources/ECSResourceDefault.java
@@ -3,14 +3,20 @@ package com.cardshifter.modapi.resources;
 public class ECSResourceDefault implements ECSResource {
 	
 	private final String name;
+    private final ResourceRetriever retriever;
 
-	public ECSResourceDefault(String name) {
+    public ECSResourceDefault(String name) {
 		this.name = name;
+        this.retriever = ResourceRetriever.forResource(this);
 	}
 	
 	@Override
 	public String toString() {
 		return name;
 	}
-	
+
+    @Override
+    public ResourceRetriever retriever() {
+        return this.retriever;
+    }
 }
