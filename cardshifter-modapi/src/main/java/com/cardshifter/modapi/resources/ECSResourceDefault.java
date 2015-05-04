266a7fb3d633871562c6dfb91a29ca0d8d9b7e8f
Simon Forsberg <zomis2k@hotmail.com>
2015-05-04 19:15:32 +0200

    added hashCode and equals for ECSResourceDefault

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/resources/ECSResourceDefault.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/resources/ECSResourceDefault.java
@@ -16,6 +16,29 @@ public class ECSResourceDefault implements ECSResource {
 	}
 
     @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ECSResourceDefault)) {
+            return false;
+        }
+
+        ECSResource that = (ECSResource) o;
+
+        if (!name.equals(that.toString())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
     public ResourceRetriever retriever() {
         return this.retriever;
     }
