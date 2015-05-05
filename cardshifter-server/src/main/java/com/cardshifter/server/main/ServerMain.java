11fb778453ec52a055be5b90005cba28a1535069
Simon Forsberg <zomis2k@hotmail.com>
2015-05-05 14:55:41 +0200

    added the option to run server without a security manager

--- cardshifter-server/src/main/java/com/cardshifter/server/main/ServerMain.java
+++ cardshifter-server/src/main/java/com/cardshifter/server/main/ServerMain.java
@@ -4,12 +4,18 @@ import org.apache.log4j.PropertyConfigurator;
 
 import com.cardshifter.server.model.MainServer;
 
+import java.util.Scanner;
+
 public class ServerMain {
 	
 	public static void main(String[] args) {
 		if (System.getSecurityManager() == null) {
 			System.out.println("No security manager has been installed, you need to install one to be protected against malicious mods, please refer to the documentation to install a correct security manager.");
-			System.exit(1);
+            System.out.println("CONTINUE AT YOUR OWN RISK");
+            System.out.println("Do you want to continue without a security manager? (y/n)");
+            if (!new Scanner(System.in).nextLine().equalsIgnoreCase("y")) {
+                System.exit(1);
+            }
 		}
 		PropertyConfigurator.configure(ServerMain.class.getResource("log4j.properties"));
 		new MainServer().start();
