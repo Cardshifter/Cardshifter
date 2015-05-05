dcc381021a32ca4d93b19d41577f271a92027d9b
Simon Forsberg <zomis2k@hotmail.com>
2015-05-05 15:05:39 +0200

    fixed reliance on default encoding

--- cardshifter-server/src/main/java/com/cardshifter/server/main/ServerMain.java
+++ cardshifter-server/src/main/java/com/cardshifter/server/main/ServerMain.java
@@ -13,7 +13,7 @@ public class ServerMain {
 			System.out.println("No security manager has been installed, you need to install one to be protected against malicious mods, please refer to the documentation to install a correct security manager.");
             System.out.println("CONTINUE AT YOUR OWN RISK");
             System.out.println("Do you want to continue without a security manager? (y/n)");
-            if (!new Scanner(System.in).nextLine().equalsIgnoreCase("y")) {
+            if (!new Scanner(System.in, "UTF-8").nextLine().equalsIgnoreCase("y")) {
                 System.exit(1);
             }
 		}
