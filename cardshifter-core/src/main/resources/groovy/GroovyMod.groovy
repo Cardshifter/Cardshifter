5098de98099fbf08e63fe217e3a5ba46ea28ddbc
Simon Forsberg <zomis2k@hotmail.com>
2015-05-16 19:57:39 +0200

    added some more Groovy mod implementation, primarily of players zones and starting resources

--- cardshifter-core/src/main/resources/groovy/GroovyMod.groovy
+++ cardshifter-core/src/main/resources/groovy/GroovyMod.groovy
@@ -1,3 +1,6 @@
+import com.cardshifter.api.config.DeckConfig
+import com.cardshifter.modapi.actions.ActionComponent
+import com.cardshifter.modapi.actions.ECSAction
 import com.cardshifter.modapi.attributes.Attributes
 import com.cardshifter.modapi.attributes.ECSAttributeMap
 import com.cardshifter.modapi.base.ComponentRetriever
@@ -5,9 +8,13 @@ import com.cardshifter.modapi.base.ECSMod;
 import com.cardshifter.modapi.base.ECSGame;
 import com.cardshifter.modapi.base.Entity
 import com.cardshifter.modapi.base.PlayerComponent
+import com.cardshifter.modapi.cards.BattlefieldComponent
+import com.cardshifter.modapi.cards.DeckComponent
+import com.cardshifter.modapi.cards.HandComponent
 import com.cardshifter.modapi.cards.ZoneComponent
 import com.cardshifter.modapi.phase.Phase
 import com.cardshifter.modapi.phase.PhaseController
+import com.cardshifter.modapi.players.Players
 import com.cardshifter.modapi.resources.ECSResource
 import com.cardshifter.modapi.resources.ECSResourceDefault
 import com.cardshifter.modapi.resources.ECSResourceMap
@@ -19,10 +26,11 @@ class CardDelegate {
     Entity entity
 
     def propertyMissing(String name, value) {
-        println "Missing property: Cannot set $name to $value"
         ECSResource res = entity.game.resource(name)
         if (res) {
             res.retriever.set(entity, (int) value)
+        } else {
+            println "Missing property: Cannot set $name to $value"
         }
     }
 
@@ -31,10 +39,11 @@ class CardDelegate {
     }
 
     def methodMissing(String name, args) {
-        println 'Missing method: ' + name
         ECSResource res = entity.game.resource(name)
         if (res) {
             res.retriever.set(entity, (int) args[0])
+        } else {
+            println 'Missing method: ' + name
         }
     }
 }
@@ -111,6 +120,31 @@ class PlayerDelegate {
     PlayerDelegate(Entity entity) {
         this.entity = entity
         entity.addComponent(config);
+        entity.addComponent(new ActionComponent())
+    }
+
+    def methodMissing(String name, args) {
+        ECSResource res = entity.game.resource(name)
+        if (res) {
+            res.retriever.set(entity, (int) args[0])
+        } else {
+            throw new UnsupportedOperationException("No such method: $name")
+        }
+    }
+
+    def endTurnAction() {
+        PhaseController phaseController = ComponentRetriever.singleton(entity.game, PhaseController)
+        entity.getComponent(ActionComponent).addAction(new ECSAction(entity, "End Turn",
+                {act -> phaseController.currentPhase.owner == entity},
+                {act -> phaseController.nextPhase()}))
+    }
+
+    def hand() {
+        entity.addComponent(new HandComponent(entity))
+    }
+
+    def battlefield() {
+        entity.addComponent(new BattlefieldComponent(entity))
     }
 
     def phase(String name) {
@@ -134,14 +168,15 @@ class PlayerDelegate {
         assert zone.name == deckConfig.zoneName
         def deck = DeckConfigFactory.create(deckConfig.minSize, deckConfig.maxSize, zone.cards, deckConfig.maxCardsPerType)
         config.addConfig("Deck", deck)
+        entity.addComponent(new DeckComponent(entity))
     }
 }
 
 public abstract class GroovyMod implements ECSMod {
 
     protected ECSGame game
-    abstract def setup()
-    abstract def config()
+    abstract def gameSetup()
+    abstract def gameConfig()
 
     ECSResource createResource(String name) {
         return new ECSResourceDefault(name)
@@ -163,7 +198,7 @@ public abstract class GroovyMod implements ECSMod {
         ECSGame.class.metaClass.players << {int count, Closure closure ->
             for (int i = 0; i < count; i++) {
                 Entity player = game.newEntity()
-                def cl = closure.rehydrate(new PlayerDelegate(player), null, null)
+                def cl = closure.rehydrate(new PlayerDelegate(player), this, this)
                 player.addComponent(new PlayerComponent(i, "Player $i"))
                 cl.call()
             }
@@ -173,17 +208,65 @@ public abstract class GroovyMod implements ECSMod {
     void declareConfiguration(ECSGame game) {
         this.game = game
         enableMeta()
-        config()
+        gameConfig()
     }
 
     void setupGame(ECSGame game) {
         this.game = game
-        setup()
+        gameSetup()
+    }
+
+    void setup(Closure<?> closure) {
+        def cl = closure.rehydrate(new SetupDelegate(this, game), this, this)
+        cl.call()
     }
 
     void game(Closure<?> closure) {
         def cl = closure.rehydrate(game, this, this)
         cl.call()
+    }
+
+}
+
+class SetupDelegate {
+    def mod
+    ECSGame game
+
+    SetupDelegate(GroovyMod mod, ECSGame game) {
+        this.mod = mod
+        this.game = game
+    }
+
+    def playerDeckFromConfig(String name) {
+        def players = Players.getPlayersInGame(game)
+        for (Entity player in players) {
+            DeckComponent deck = player.getComponent(DeckComponent)
+            ConfigComponent playerConfig = player.getComponent(ConfigComponent)
+            DeckConfig config = playerConfig.getConfigs().get(name)
+            setupDeck(deck, config)
+        }
+    }
+
+    def setupDeck(DeckComponent deck, DeckConfig deckConf) {
+        def game = deck.owner.game;
+        for (def chosen in deckConf.chosen.entrySet()) {
+            def entityId = chosen.key;
+            def count = chosen.value;
+
+            for (int i = 0; i < count; i++) {
+                def existing = game.getEntity(entityId);
+                def copy = existing.copy();
+                deck.addOnBottom(copy);
+            }
+        }
+    }
+
+    def playerDeckShuffle() {
+        def players = Players.getPlayersInGame(game)
+        for (Entity player in players) {
+            DeckComponent deck = player.getComponent(DeckComponent)
+            deck.shuffle()
+        }
     }
 
 }
--- extra-resources/groovy/game1/Game.groovy
+++ extra-resources/groovy/game1/Game.groovy
@@ -1,6 +1,13 @@
 import GroovyMod;
 import Cards
 
+def ownedBattlefieldCreatures = {entity ->
+    def Cards = Java.type("com.cardshifter.modapi.cards.Cards");
+    return entity.hasComponent(com.cardshifter.modapi.base.CreatureTypeComponent.class) &&
+            Cards.isOnZone(entity, com.cardshifter.modapi.cards.BattlefieldComponent.class) &&
+            Cards.isOwnedByCurrentPlayer(entity)
+}
+
 class MyGame extends GroovyMod {
 
     def ATTACK = createResource('ATTACK')
@@ -14,10 +21,17 @@ class MyGame extends GroovyMod {
     def MANA_MAX = createResource("MANA_MAX")
     def SICKNESS = createResource("SICKNESS")
     def TAUNT = createResource("TAUNT")
+    def SCRAP = createResource("SCRAP")
 
-    def config() {
+    def PLAY_ACTION = "Play";
+    def ENCHANT_ACTION = "Enchant";
+    def ATTACK_ACTION = "Attack";
+    def END_TURN_ACTION = "End Turn";
+    def USE_ACTION = "Use";
+
+    def gameConfig() {
         resources([ATTACK, HEALTH, MAX_HEALTH, ATTACK_AVAILABLE, DENY_COUNTERATTACK, MANA, MANA_COST, MANA_MAX,
-                SICKNESS, TAUNT])
+                SICKNESS, TAUNT, SCRAP])
         game {
             println 'Game Closure!'
             neutral {
@@ -38,174 +52,106 @@ class MyGame extends GroovyMod {
                         zone 'Cards'
                     }
                 }
+                endTurnAction()
+                hand()
+                battlefield()
+                health 30
+                max_health 30
+                mana 0
+                scrap 0
             }
         }
     }
 
-    def setup() {
+    def gameSetup() {
         println 'setup game called ' + game
+        setup {
+            playerDeckFromConfig('Deck')
+            playerDeckShuffle()
+
+
+
+        }
+
+        def LastPlayersStandingEndsGame = net.zomis.cardshifter.ecs.usage.LastPlayersStandingEndsGame
+        def EffectActionSystem = net.zomis.cardshifter.ecs.effects.EffectActionSystem
+
+        def removeDead = {
+            game.events.registerHandlerAfter(this, com.cardshifter.modapi.actions.ActionPerformEvent.class, {event ->
+                def battlefield = com.cardshifter.modapi.cards.BattlefieldComponent.class;
+                def remove = event.entity.game.getEntitiesWithComponent(battlefield)
+                        .stream().flatMap({entity -> entity.getComponent(battlefield).stream()})
+                        .peek({e -> print(e + " has " + HEALTH.getFor(e))})
+                        .filter({e -> HEALTH.getFor(e) <= 0})
+                        .collect(java.util.stream.Collectors.toList());
+                for (def e in remove) {
+                    e.destroy();
+                }
+            })
+        }
+
+/*        systems {
+            gainResource(res: MANA_MAX, value: 1, untilMax: 10)
+            restoreResources(res: MANA, value: { res MANA_MAX })
+
+            // Play
+            playFromHand PLAY_ACTION
+            playEntersBattlefield PLAY_ACTION
+            useCost(action: PLAY_ACTION, res: MANA, value: { res MANA_COST }, whoPays: "player")
+
+            // Enchant
+            playFromHand ENCHANT_ACTION
+            EnchantTargetCreatureTypes("Bio")
+            EffectActionSystem(ENCHANT_ACTION) // needs to be before EnchantPerform, because of entity removal
+            EnchantPerform(ATTACK, HEALTH, MAX_HEALTH)
+
+            // Spell
+            useCost(action: USE_ACTION, res: MANA, value: { res MANA_COST }, whoPays: "player")
+            playFromHand USE_ACTION,
+                    EffectActionSystem(USE_ACTION)
+            EffectActionSystem(PLAY_ACTION)
+            targetFilterSystem USE_ACTION
+            destroyAfterUse USE_ACTION
+
+            // Attack
+            AttackOnBattlefield()
+            AttackTargetMinionsFirstThenPlayer(TAUNT)
+            AttackSickness(SICKNESS)
+            useCost(action: ATTACK_ACTION, res: ATTACK_AVAILABLE, value: 1, whoPays: "self")
+
+
+            RestoreResourcesToSystem(ownedBattlefieldCreatures, ATTACK_AVAILABLE, {ent -> 1})
+            RestoreResourcesToSystem(ownedBattlefieldCreatures, SICKNESS, {ent -> Math.max(0, SICKNESS.getFor(ent) - 1)})
+            TrampleSystem(HEALTH)
+
+            // Draw cards
+            startCards 5
+            DrawCardAtBeginningOfTurnSystem()
+            DamageConstantWhenOutOfCardsSystem(HEALTH, 1)
+            LimitedHandSizeSystem(10, {card -> card.getCardToDraw().destroy()})
+
+            // General setup
+            MulliganSingleCards(game)
+            GameOverIfNoHealth(HEALTH)
+            LastPlayersStandingEndsGame()
+            RemoveDeadEntityFromZoneSystem()
+            PerformerMustBeCurrentPlayer()
+            removeDead()
+            ResourceRecountSystem()
+        }
+
+        def allowCounterAttackRes = DENY_COUNTERATTACK.retriever;
+        def allowCounterAttack = function (attacker, defender) {
+            return allowCounterAttackRes.getOrDefault(attacker, 0) == 0;
+        }
+
+        game.addSystem(AttackDamageAccumulating(ATTACK, HEALTH, allowCounterAttack))
+        game.addSystem(AttackDamageHealAtEndOfTurn(HEALTH, MAX_HEALTH))
+
+        game.addSystem(ApplyAfterAttack({entity -> allowCounterAttackRes.getFor(entity) > 0}),
+                { entity -> SICKNESS.retriever.set(entity, 2) })*/
     }
 
 }
 
 new MyGame()
-
-/*
-def PLAY_ACTION = "Play";
-def ENCHANT_ACTION = "Enchant";
-def ATTACK_ACTION = "Attack";
-def END_TURN_ACTION = "End Turn";
-def USE_ACTION = "Use";
-
-def ownedBattlefieldCreatures = {entity ->
-    def Cards = Java.type("com.cardshifter.modapi.cards.Cards");
-    return entity.hasComponent(com.cardshifter.modapi.base.CreatureTypeComponent.class) &&
-        Cards.isOnZone(entity, com.cardshifter.modapi.cards.BattlefieldComponent.class) &&
-        Cards.isOwnedByCurrentPlayer(entity)
-}
-
-def playerSetup(game) {
-    def phaseController = new com.cardshifter.modapi.phase.PhaseController();
-    game.newEntity().addComponent(phaseController);
-
-    def players = com.cardshifter.modapi.players.Players.getPlayersInGame(game);
-    for (int i = 0; i < 2; i++) {
-        int playerIndex = i;
-        def player = players.get(i);
-        def playerPhase = new com.cardshifter.modapi.phase.Phase(player, "Main");
-        phaseController.addPhase(playerPhase);
-
-        def actions = new com.cardshifter.modapi.actions.ActionComponent();
-        player.addComponent(actions);
-
-        def isPhase = function (phase) {
-            return function (act) {
-                var check = phaseController.getCurrentPhase() == phase;
-                return phaseController.getCurrentPhase() == phase;
-            }
-        }
-
-        def endTurnAction = new com.cardshifter.modapi.actions.ECSAction(player, END_TURN_ACTION,
-                isPhase(playerPhase), { act -> phaseController.nextPhase(); })
-        actions.addAction(endTurnAction);
-
-        com.cardshifter.modapi.resources.ECSResourceMap.createFor(player)
-                .set(HEALTH, 30)
-                .set(MAX_HEALTH, 30)
-                .set(MANA, 0)
-                .set(SCRAP, 0);
-
-        def deck = new com.cardshifter.modapi.cards.DeckComponent(player);
-        def hand = new com.cardshifter.modapi.cards.HandComponent(player);
-        def battlefield = new com.cardshifter.modapi.cards.BattlefieldComponent(player);
-        player.addComponents(hand, deck, battlefield);
-
-        def ConfigComponent = Java.type("net.zomis.cardshifter.ecs.config.ConfigComponent");
-        def config = player.getComponent(ConfigComponent.class);
-        def deckConf = config.getConfig(com.cardshifter.api.config.DeckConfig.class);
-        if (deckConf.total() < deckConf.getMinSize()) {
-            deckConf.generateRandom();
-        }
-        setupDeck(deck, deckConf);
-        deck.shuffle();
-    }
-}
-
-def setupDeck(deck, deckConf) {
-    def game = deck.owner.game;
-    for (def chosen in deckConf.chosen.entrySet()) {
-        def entityId = chosen.key;
-        def count = chosen.value;
-
-        for (int i = 0; i < count; i++) {
-            var existing = game.getEntity(entityId);
-            var copy = existing.copy();
-            deck.addOnBottom(copy);
-        }
-    }
-}
-
-
-def setupGame2(game) {
-    playerSetup(game)
-
-    var LastPlayersStandingEndsGame = Java.type("net.zomis.cardshifter.ecs.usage.LastPlayersStandingEndsGame");
-    var EffectActionSystem = Java.type("net.zomis.cardshifter.ecs.effects.EffectActionSystem");
-
-    def removeDead = {
-        game.events.registerHandlerAfter(this, com.cardshifter.modapi.actions.ActionPerformEvent.class, {event ->
-            def battlefield = com.cardshifter.modapi.cards.BattlefieldComponent.class;
-            def remove = event.entity.game.getEntitiesWithComponent(battlefield)
-                .stream().flatMap({entity -> entity.getComponent(battlefield).stream()})
-                .peek({e -> print(e + " has " + HEALTH.getFor(e))})
-                .filter({e -> HEALTH.getFor(e) <= 0})
-                .collect(java.util.stream.Collectors.toList());
-                for (def e in remove) {
-                    e.destroy();
-                }
-            }
-        )
-    }
-
-    systems {
-        gainResource(res: MANA_MAX, value: 1, untilMax: 10)
-        restoreResources(res: MANA, value: { res MANA_MAX })
-
-        // Play
-        playFromHand PLAY_ACTION
-        playEntersBattlefield PLAY_ACTION
-        useCost(action: PLAY_ACTION, res: MANA, value: { res MANA_COST }, whoPays: "player")
-
-        // Enchant
-        playFromHand ENCHANT_ACTION
-        EnchantTargetCreatureTypes("Bio")
-        EffectActionSystem(ENCHANT_ACTION) // needs to be before EnchantPerform, because of entity removal
-        EnchantPerform(ATTACK, HEALTH, MAX_HEALTH)
-
-        // Spell
-        useCost(action: USE_ACTION, res: MANA, value: { res MANA_COST }, whoPays: "player")
-        playFromHand USE_ACTION,
-        EffectActionSystem(USE_ACTION)
-        EffectActionSystem(PLAY_ACTION)
-        targetFilterSystem USE_ACTION
-        destroyAfterUse USE_ACTION
-
-        // Attack
-        AttackOnBattlefield()
-        AttackTargetMinionsFirstThenPlayer(TAUNT)
-        AttackSickness SICKNESS
-        useCost(action: ATTACK_ACTION, res: ATTACK_AVAILABLE, value: 1, whoPays: "self")
-
-
-        RestoreResourcesToSystem(ownedBattlefieldCreatures, ATTACK_AVAILABLE, {ent -> 1})
-        RestoreResourcesToSystem(ownedBattlefieldCreatures, SICKNESS, {ent -> Math.max(0, SICKNESS.getFor(ent) - 1)})
-        TrampleSystem HEALTH
-
-        // Draw cards
-        startCards 5
-        DrawCardAtBeginningOfTurnSystem()
-        DamageConstantWhenOutOfCardsSystem(HEALTH, 1)
-        LimitedHandSizeSystem(10, {card -> card.getCardToDraw().destroy()})
-
-        // General setup
-        MulliganSingleCards(game)
-        GameOverIfNoHealth(HEALTH)
-        LastPlayersStandingEndsGame()
-        RemoveDeadEntityFromZoneSystem()
-        PerformerMustBeCurrentPlayer()
-        removeDead()
-        ResourceRecountSystem()
-    }
-
-    def allowCounterAttackRes = DENY_COUNTERATTACK.retriever;
-    def allowCounterAttack = function (attacker, defender) {
-        return allowCounterAttackRes.getOrDefault(attacker, 0) == 0;
-    }
-
-    game.addSystem(AttackDamageAccumulating(ATTACK, HEALTH, allowCounterAttack))
-    game.addSystem(AttackDamageHealAtEndOfTurn(HEALTH, MAX_HEALTH))
-
-    game.addSystem(ApplyAfterAttack({entity -> allowCounterAttackRes.getFor(entity) > 0}),
-        { entity -> SICKNESS.retriever.set(entity, 2) })
-}
-*/
