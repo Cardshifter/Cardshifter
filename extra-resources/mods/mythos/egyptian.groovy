01d44ced13301b73f7e32ba07dc7351c1b1de64d
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-05-18 16:32:26 -0400

    Create egyptian.groovy

---
+++ extra-resources/mods/mythos/egyptian.groovy
@@ -0,0 +1,100 @@
+/* See modding documentation for all available keywords. */
+/* GENERIC TEMPLATE
+card('ChangeMe') {
+    creature "Egyptian"
+    flavor "hello"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+*/
+
+card('AMUN-RA') {
+    creature "Egyptian"
+    flavor "The supreme god and god of the sun."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('ISIS') {
+    creature "Egyptian"
+    flavor "Goddess of motherhood, family and royalty."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('HATHOR') {
+    creature "Egyptian"
+    flavor "Goddess of love and happiness."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('BAST') {
+    creature "Egyptian"
+    flavor "Cat-headed goddess of protection and fertility."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('TOTH') {
+    creature "Egyptian"
+    flavor "God of art, science, astronomy and literature."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('ANUBIS') {
+    creature "Egyptian"
+    flavor "Dog-headed god of funerals and the dead."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('OSIRIS') {
+    creature "God of the underworld."
+    flavor "hello"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('SHU') {
+    creature "Egyptian"
+    flavor "God of air and the sky."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('HORUS') {
+    creature "Egyptian"
+    flavor "Falcon-head"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('SET') {
+    creature "Egyptian"
+    flavor "God of anarchy and chaos."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('SEKHMET') {
+    creature "Egyptian"
+    flavor "Lion-headed goddess of war and vengeance."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
