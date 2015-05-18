c9d38d71380e5fb1aaee18a836efca4e419cc129
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-05-18 19:59:48 -0400

    added Chinese cards

--- extra-resources/mods/mythos/chinese.groovy
+++ extra-resources/mods/mythos/chinese.groovy
@@ -9,9 +9,17 @@ card('ChangeMe') {
     attack 1
 }
 */
-card('JADE-EMPEROR') {
+card('JADE EMPEROR') {
     creature "Chinese"
     flavor "Creator and ruler of the universe."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('FENG-DU') {
+    creature "Chinese"
+    flavor "The Realm of the Dead, containing all the Hells."
     health 1
     sickness 1
     manaCost 1
@@ -28,6 +36,54 @@ card('MONKING') {
 card('GUAN-YU') {
     creature "Chinese"
     flavor "God of War and Martial Arts"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('EIGHT IMMORTALS') {
+    creature "Chinese"
+    flavor "Accidental immortals, gods and goddesses of perfection."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('YEN-LO-WANG') {
+    creature "Chinese"
+    flavor "Deity of Death and Lord of the Fifth Floor of Hell"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('GUAN-YIN') {
+    creature "Chinese"
+    flavor "Goddess of Compassion and Caring"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('AO-CHIN') {
+    creature "Chinese"
+    flavor "Sea Spirit. King of the Southern Ocean"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('NU-GUA') {
+    creature "Chinese"
+    flavor "Serpent Creator Goddess of Mankind, Marriage and Mud"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('CAO-GUOJIU') {
+    creature "Chinese"
+    flavor "Immortal theatre fan and royal patron of actors"
     health 1
     sickness 1
     manaCost 1
