a8392e4fc343b3fcdbc8d315942843c392a40ab1
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-05-17 20:18:31 -0400

    added Greek mythos

---
+++ extra-resources/mods/mythos/greek.groovy
@@ -0,0 +1,139 @@
+/* See modding documentation for all available keywords. */
+/* GENERIC TEMPLATE
+card('Change Me') {
+    creature "Greek"
+    flavor "hello"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+*/
+card('OLYMPUS') {
+    creature "Greek"
+    flavor "A lofty mountain where all the top Greek deities have their palaces and parties"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('ZEUS') {
+    creature "Greek"
+    flavor "God of the sky. Ruler of Mount Olympus."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+
+card('ATLAS') {
+    creature "Greek"
+    flavor "God of strength, weightlifting and heavy burdens."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('ARES') {
+    creature "Greek"
+    flavor "Hot-blooded god of war. "
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('APHRODITE') {
+    creature "Greek"
+    flavor "Goddess of love."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('HADES') {
+    creature "Greek"
+    flavor "God of death."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('CHAOS') {
+    creature "Greek"
+    flavor "Goddess of nothingness, empty space and confusion."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+card('HERMES') {
+    creature "Greek"
+    flavor "God of merchants and entrepreneurs."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('URANUS') {
+    creature "Greek"
+    flavor "God of the sky and father of the Titans"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('DEMETER') {
+    creature "Greek"
+    flavor "Goddess of farming."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('DIONYSUS') {
+    creature "Greek"
+    flavor "God of Sex, Wine and Intoxication."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('ARTEMIS') {
+    creature "Greek"
+    flavor "Goddess of hunting."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('POSEIDON') {
+    creature "Greek"
+    flavor "God of the sea and patron of sailors"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('ERIS') {
+    creature "Greek"
+    flavor "Goddess of strife and discord."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('HEPHAESTUS') {
+    creature "Greek"
+    flavor "God of Blacksmithing"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('MEDUSA') {
+    creature "Greek"
+    flavor "Demonness of the Gorgons, the hideous snake-haired hags."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
