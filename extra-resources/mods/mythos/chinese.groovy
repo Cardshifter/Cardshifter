73a87479e49c15352dd5afe60e09de25da677457
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-05-18 18:00:28 -0400

    Create chinese.groovy

---
+++ extra-resources/mods/mythos/chinese.groovy
@@ -0,0 +1,35 @@
+/* See modding documentation for all available keywords. */
+/* GENERIC TEMPLATE
+card('ChangeMe') {
+    creature "Chinese"
+    flavor "hello"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+*/
+card('JADE-EMPEROR') {
+    creature "Chinese"
+    flavor "Creator and ruler of the universe."
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('MONKING') {
+    creature "Chinese"
+    flavor "The infamous irrepressible Monkey King and Great Sage Equal Of Heaven"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+card('GUAN-YU') {
+    creature "Chinese"
+    flavor "God of War and Martial Arts"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
