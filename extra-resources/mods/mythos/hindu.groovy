c6363e4ffd91b8b61ac264cdba2bd9412b75679b
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-05-20 13:20:13 -0400

    Create hindu.groovy

---
+++ extra-resources/mods/mythos/hindu.groovy
@@ -0,0 +1,10 @@
+/* See modding documentation for all available keywords. */
+/* GENERIC TEMPLATE
+card('ChangeMe') {
+    creature "Hindu"
+    flavor "hello"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
