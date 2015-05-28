8d41f74d84413be4428c2d3a80592023563208e5
Simon Forsberg <zomis2k@hotmail.com>
2015-05-28 20:50:08 +0200

    added set resource modifier, fixes #236

--- cardshifter-core/src/main/resources/groovy/WhilePresentDelegate.groovy
+++ cardshifter-core/src/main/resources/groovy/WhilePresentDelegate.groovy
@@ -10,6 +10,7 @@ import com.cardshifter.modapi.resources.ECSResourceBiStrategy
 import com.cardshifter.modapi.resources.EntityModifier
 
 import java.util.function.BiPredicate
+import java.util.function.Function
 import java.util.function.Predicate
 
 class ModifierDelegate {
@@ -107,31 +108,51 @@ class WhilePresentDelegate {
     List<ModifierDelegate> modifiers = new ArrayList<>()
     StringBuilder description = new StringBuilder()
 
+    private addModifier(ECSResource[] resources, int priority, Closure filter,
+            Function<FilterDelegate, String> stringFunction, ECSResourceBiStrategy strategy) {
+        FilterDelegate deleg = new FilterDelegate()
+        filter.delegate = deleg
+        filter.call()
+        Predicate<Entity> active = {!it.isRemoved()}
+        BiPredicate<Entity, Entity> appliesTo = deleg.predicate
+
+        Closure<EntityModifier> closure = {Entity entity ->
+            new EntityModifier(entity, priority, active, appliesTo, strategy)
+        }
+
+        for (ECSResource res : resources) {
+            ModifierDelegate modifier = new ModifierDelegate(resource: res, modifierCreator: closure)
+            modifiers.add(modifier)
+        }
+
+        String desc = stringFunction.apply(deleg)
+        description.append(desc)
+        description.append('\n')
+    }
+
     def change(ECSResource... resources) {
         [by: {int change ->
             [withPriority: {int priority ->
                 [onCards: {Closure filter ->
-                    FilterDelegate deleg = new FilterDelegate()
-                    filter.delegate = deleg
-                    filter.call()
-                    Predicate<Entity> active = {!it.isRemoved()}
-                    BiPredicate<Entity, Entity> appliesTo = deleg.predicate
-                    ECSResourceBiStrategy amount = {Entity source, Entity target, ECSResource resource, int actualValue ->
+                    addModifier(resources, priority, filter, {deleg ->
+                        'Give ' + deleg.description + ' ' + change + ' ' + resources.join(' and ')
+                    }, {Entity source, Entity target, ECSResource resource, int actualValue ->
                         actualValue + change
-                    }
+                    })
+                }]
+            }]
+        }]
+    }
 
-                    Closure<EntityModifier> closure = {Entity entity ->
-                        new EntityModifier(entity, priority, active, appliesTo, amount)
-                    }
-
-                    for (ECSResource res : resources) {
-                        ModifierDelegate modifier = new ModifierDelegate(resource: res, modifierCreator: closure)
-                        modifiers.add(modifier)
-                    }
-
-                    String desc = "Give " + deleg.description + " " + change + " " + resources.join(' and ')
-                    description.append(desc)
-                    description.append('\n')
+    def set(ECSResource... resources) {
+        [to: {int change ->
+            [withPriority: {int priority ->
+                [onCards: {Closure filter ->
+                    addModifier(resources, priority, filter, { deleg ->
+                        'Set ' + resources.join(' and ') + ' to ' + deleg.description + ' ' + change
+                    }, {Entity source, Entity target, ECSResource resource, int actualValue ->
+                          change
+                    })
                 }]
             }]
         }]
