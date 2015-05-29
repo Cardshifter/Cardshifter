d0c4d13cdc464462b93f2032ce0673144d19eb29
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-05-29 13:22:01 -0400

    Create common.groovy

---
+++ extra-resources/groovy/mythos/common.groovy
@@ -0,0 +1,17 @@
+package mythos
+/* See modding documentation for all available keywords. */
+/* GENERIC TEMPLATE
+card("ChangeMe") {
+    creature "Common"
+    flavor "hello"
+    health 1
+    sickness 1
+    manaCost 1
+    attack 1
+}
+*/
+
+/**
+ * List of available common cards Cardshifter "Mythos" mod
+ * @author www.github.com/Phrancis
+ */
