352b30c68981af40f013656168b348801fed4204
Simon Forsberg <zomis2k@hotmail.com>
2015-05-31 16:11:32 +0200

    added hashcode and equals for ECSAttributeDefault

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/attributes/ECSAttributeDefault.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/attributes/ECSAttributeDefault.java
@@ -13,4 +13,20 @@ public class ECSAttributeDefault implements ECSAttribute {
 		return name;
 	}
 	
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ECSAttributeDefault that = (ECSAttributeDefault) o;
+
+        if (!name.equals(that.name)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
