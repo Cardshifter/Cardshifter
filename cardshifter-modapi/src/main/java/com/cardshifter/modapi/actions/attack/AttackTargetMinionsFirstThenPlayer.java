0b9f97f8a8d102da4d62e206d385f13798c63601
Simon Forsberg <zomis2k@hotmail.com>
2015-06-01 21:18:36 +0200

    fixed bug where one creature having taunt caused the same behavior as all creatures having taunt

--- cardshifter-modapi/src/main/java/com/cardshifter/modapi/actions/attack/AttackTargetMinionsFirstThenPlayer.java
+++ cardshifter-modapi/src/main/java/com/cardshifter/modapi/actions/attack/AttackTargetMinionsFirstThenPlayer.java
@@ -8,17 +8,29 @@ import com.cardshifter.modapi.cards.BattlefieldComponent;
 import com.cardshifter.modapi.cards.CardComponent;
 import com.cardshifter.modapi.cards.Cards;
 import com.cardshifter.modapi.phase.PhaseController;
+import com.cardshifter.modapi.players.Players;
 import com.cardshifter.modapi.resources.ECSResource;
 import com.cardshifter.modapi.resources.ResourceRetriever;
+
+import java.util.Objects;
+import java.util.function.Predicate;
 
 public class AttackTargetMinionsFirstThenPlayer extends SpecificActionTargetSystem {
 
 	private final ResourceRetriever attackFirstResource;
+    private final Predicate<Entity> shouldAttackFirst;
 
 	public AttackTargetMinionsFirstThenPlayer(ECSResource requiredResource) {
 		super("Attack");
 		this.attackFirstResource = ResourceRetriever.forResource(requiredResource);
+        this.shouldAttackFirst = e -> attackFirstResource.getOrDefault(e, 0) > 0;
 	}
+
+    private boolean hasTauntMinions(Entity player) {
+        BattlefieldComponent battlefieldComponent = player.getComponent(BattlefieldComponent.class);
+        Objects.requireNonNull(battlefieldComponent, "Entity does not have a battlefield: " + player.debug());
+        return battlefieldComponent.stream().anyMatch(shouldAttackFirst);
+    }
 	
 	protected void checkTargetable(TargetableCheckEvent event) {
 		Entity target = event.getTarget();
@@ -28,15 +40,19 @@ public class AttackTargetMinionsFirstThenPlayer extends SpecificActionTargetSyst
 			}
 			if (!Cards.isOnZone(target, BattlefieldComponent.class)) {
 				event.setAllowed(false);
+                return;
 			}
+            if (!shouldAttackFirst.test(target) && hasTauntMinions(Players.findOwnerFor(target))) {
+                event.setAllowed(false);
+            }
 		}
 		else if (target.hasComponent(PlayerComponent.class)) {
 			if (target == ComponentRetriever.singleton(target.getGame(), PhaseController.class).getCurrentEntity()) {
 				event.setAllowed(false);
 			}
-			if (target.getComponent(BattlefieldComponent.class).stream().anyMatch(e -> attackFirstResource.getOrDefault(e, 0) > 0)) {
-				event.setAllowed(false);
-			}
+            if (hasTauntMinions(target)) {
+                event.setAllowed(false);
+            }
 		}
 		else {
 			event.setAllowed(false);
