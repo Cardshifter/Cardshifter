49e8c5575a9a2dd1b80b5dc0caf9a3a5c5de74d1
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-06-04 12:53:59 -0400

    Add Zeus, Hades, Cronus, some tokens

--- extra-resources/mods/Mythos/greek.groovy
+++ extra-resources/mods/Mythos/greek.groovy
@@ -5,138 +5,101 @@ card('Change Me') {
     creature "Greek"
     flavor "hello"
     health 1
-    sickness 1
-    manaCost 1
     attack 1
+    manaCost 1
+    sickness 1
 }
 */
 
-card('OLYMPUS') {
-    creature "Greek"
-    flavor "A lofty mountain where all the top Greek deities have their palaces and parties"
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
+// CREATURES
+
 card('ZEUS') {
-    creature "Greek"
-    flavor "God of the sky. Ruler of Mount Olympus."
-    health 1
+    creature "Greek God"
+    flavor "God of the sky, ruler of Mount Olympus."
+    health 8
+    attack 8
+    manaCost 30
     sickness 1
-    manaCost 1
-    attack 1
+
+    denyCounterAttack() // ranged
+    /*
+    pickAction {
+        // (3) Deal +1 damage to target creature or player per turn.
+        { damage 1 to target cost 3 mana }
+        // (5) Deal +5 damage to target creature or player per turn.
+        { damage 3 to target cost 3 mana }
+    }
+    */
 }
 
-card('ATLAS') {
-    creature "Greek"
-    flavor "God of strength, weightlifting and heavy burdens."
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
-card('ARES') {
-    creature "Greek"
-    flavor "Hot-blooded god of war. "
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
-card('APHRODITE') {
-    creature "Greek"
-    flavor "Goddess of love."
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
 card('HADES') {
-    creature "Greek"
-    flavor "God of death."
-    health 1
+    creature "Greek Chthonic God"
+    flavor "God of the Underworld."
+    health 8
+    attack 8
+    manaCost 30
     sickness 1
-    manaCost 1
-    attack 1
+    // plague()
+    // Add +1/+1 to all Chthonic Deities on the field.
+    whilePresent {
+        change ATTACK, HEALTH by 1 priority 1 onCards {
+            creatureType "Greek Chthonic God"
+            zone "Battlefield"
+            owner "all"
+        }
+    }
+    // When Hades is sent to the graveyard, add three +1/+1 Lost Soul tokens to the field.
+    onDeath {
+        summon 3 of "Lost Soul" to "you" zone "Battlefield"
+    }
 }
-card('CHAOS') {
-    creature "Greek"
-    flavor "Goddess of nothingness, empty space and confusion."
-    health 1
+
+card('CRONUS') {
+    creature "Greek Titan"
+    flavor "Deposed King of the Titans; also father of Zeus, Hades, and Poseidon."
+    health 8
+    attack 4
+    manaCost 30
     sickness 1
-    manaCost 1
-    attack 1
+    // phase()
+    // When this creature is put into play, add one +2/+2 Titan Token to the field.
+    afterPlay {
+        summon 1 of "Titan 2/2" to "you" zone "Battlefield"
+    }
+    // When this creature is sent to the graveyard, add one +4/+2 Titan Token to the Field.
+    onDeath {
+        summon 1 of "Titan 4/2" to "you" zone "Battlefield"
+    }
 }
-card('HERMES') {
+
+
+
+// TOKENS
+
+card('Lost Soul') {
     creature "Greek"
-    flavor "God of merchants and entrepreneurs."
+    flavor ""
     health 1
-    sickness 1
-    manaCost 1
     attack 1
+    manaCost 0
+    sickness 1
+    token()
 }
-card('URANUS') {
-    creature "Greek"
-    flavor "God of the sky and father of the Titans"
-    health 1
+card('Titan 2/2') {
+    creature "Greek Titan"
+    flavor ""
+    health 2
+    attack 2
+    manaCost 0
     sickness 1
-    manaCost 1
-    attack 1
+    token()
 }
-card('DEMETER') {
-    creature "Greek"
-    flavor "Goddess of farming."
-    health 1
+card('Titan 4/2') {
+    creature "Greek Titan"
+    flavor ""
+    health 2
+    attack 4
+    manaCost 0
     sickness 1
-    manaCost 1
-    attack 1
-}
-card('DIONYSUS') {
-    creature "Greek"
-    flavor "God of Sex, Wine and Intoxication."
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
-card('ARTEMIS') {
-    creature "Greek"
-    flavor "Goddess of hunting."
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
-card('POSEIDON') {
-    creature "Greek"
-    flavor "God of the sea and patron of sailors"
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
-card('ERIS') {
-    creature "Greek"
-    flavor "Goddess of strife and discord."
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
-card('HEPHAESTUS') {
-    creature "Greek"
-    flavor "God of Blacksmithing"
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
-}
-card('MEDUSA') {
-    creature "Greek"
-    flavor "Demonness of the Gorgons, the hideous snake-haired hags."
-    health 1
-    sickness 1
-    manaCost 1
-    attack 1
+    token()
 }
