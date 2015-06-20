b06e97aac497db61910a95a3c5d9ed7edbbd0aa9
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-06-20 15:16:24 -0400

    JavaFX UI changes, and some GroovyDoc

--- extra-resources/mods/Mythos/Game.groovy
+++ extra-resources/mods/Mythos/Game.groovy
@@ -64,7 +64,8 @@ config {
         resourceModifier()
         phases()
         /**
-         * List of cardsets to load into the Cards zone, which is the pool of all possible cards available.
+         * List of cardsets to load into the game entity.
+         * Note: The 'Cards' zone contains all cards available to the game entity.
          */
         zone('Cards') {
             cardset 'common'
@@ -78,14 +79,20 @@ config {
     }
 
     /**
-     * General player configuration
+     * Player configuration
+     * Creates identical configuration for each player.
      */
+
+    // Note: As of current time 2015-06-18, only two players are supported. This entry will be updated if/when this changes.
     players(2) {
         phase 'Main'
         config {
             deck {
+                // minimum cards in deck
                 minSize 30
+                // maximum cards in deck
                 maxSize 30
+                // maximum number of copies of a card - can be overridden by the maxInDeck property on individual cards
                 maxCardsPerType 3
                 zone 'Cards'
             }
@@ -93,23 +100,62 @@ config {
         endTurnAction()
         hand()
         battlefield()
+        // players' starting health and maximum health
         health 30
         max_health 30
+        // initializing players' mana and mana max, which are set in the "setup" method in this file
         mana 0
         mana_max 0
     }
 }
 
 setup {
+
+    // Add selected cards to player deck.
     playerDeckFromConfig('Deck')
+
+    // Shuffle the cards in the decks. If you disable this, cards will not be shuffled
+    // and instead be in the same order each time the deck is loaded.
     playerDeckShuffle()
 
+    /**
+     * THIS "systems" SECTION IS VERY IMPACTING TO HOW THE GAME PLAYS!
+     * PLEASE READ THE DOCUMENTATION CAREFULLY!
+     * This section is in the works to be improved and made easier to use, see this issue:
+     * https://github.com/Cardshifter/Cardshifter/issues/247
+     */
+
     systems {
+        /**
+         * Defines how player resources are handled at the beginning of the game and as turns go by.
+         * @param res The resource to be gained.
+         * @param value The initial value on the first turn of each player.
+         * @param untilMax The maximum value that the resource will reach.
+         */
         gainResource(res: MANA_MAX, value: 10, untilMax: 100)
+
+        /**
+         * Defines the resource to be restored at the beginning of each turn.
+         * @param resource The resource to be restored.
+         * @param value The value to restore the resource to.
+         */
         restoreResources(resource: MANA, value: MANA_MAX)
+
+        /**
+         * Declares upkeep cost of cards.
+         * Upkeep cost is defined as the "tax" on a resource of having a card in a zone.
+         * Generally used for creatures that you own on the Battlefield, but can be used for other things.
+         * If the mod does not use this mechanic, this can be disabled.
+         * @param filter Refers to the name of the method/closure that filters cards.
+         * @param decreaseBy Refers to the value by which to decrease the resource.
+         * @param decrease Refers to which resource is being decreased by "decreaseBy" param.
+         */
         upkeepCost(filter: ownedBattlefieldCreatures, decreaseBy: MANA_COST, decrease: MANA)
 
-        // Play
+        /**
+         * "PLAY" ACTION DEFINITION
+         */
+        // Declares the play action
         playFromHand PLAY_ACTION
         useCost(action: PLAY_ACTION, res: MANA, value: MANA_COST, whoPays: "player")
         playEntersBattlefield PLAY_ACTION
