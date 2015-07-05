eca2c070f0df63665f08884bf71e2b70f70ce908
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-07-05 18:48:14 -0400

    cleaned up cyborg-chronicles/enchantments.groovy

--- extra-resources/mods/Cyborg-Chronicles/enchantments.groovy
+++ extra-resources/mods/Cyborg-Chronicles/enchantments.groovy
@@ -1,71 +1,70 @@
 card('Bionic Arms') {
+    flavor "These arms will give strength to even the most puny individual."
     enchantment true
     addAttack 2
     scrapCost 1
-    flavor "These arms will give strength to even the most puny individual."
-
 }
 
 card('Body Armor') {
+    flavor "Steel-reinforced armor to absord damage from blows and shots."
     enchantment true
     addHealth 2
     scrapCost 1
-    flavor "Steel-reinforced armor to absord damage from blows and shots."
 }
 
 card('Adrenalin Injection') {
+    flavor "An injection to increase speed and body function."
     enchantment true
-    set(SICKNESS, 0)
+    set(SICKNESS, 0) // give "Rush"
     addAttack 1
     addHealth 1
     scrapCost 1
-    flavor "An injection to increase speed and body function."
 }
 
 card('Steroid Implants') {
+    flavor "Intravenous implants that feed the body for increased strength."
     enchantment true
     addAttack 2
     addHealth 1
     scrapCost 2
-    flavor "Intraveneous implants that feed the body for increased strength."
 }
 
 card('Reinforced Cranial Implants') {
+    flavor "Offers head protection as well as a slight increase in brain activity."
     enchantment true
     addAttack 1
     addHealth 2
     scrapCost 2
-    flavor "Offers head protection as well as a slight increase in brain activity."
 }
 
 card('Cybernetic Arm Cannon') {
+    flavor "Replaces the forearm with a powerful firearm for massive damage."
     enchantment true
-    set(DENY_COUNTERATTACK, 1)
+    set(DENY_COUNTERATTACK, 1) // give "Ranged"
     addAttack 3
     addHealth 0
     scrapCost 2
-    flavor "Replaces the forearm with a powerful firearm for massive damage."
 }
 
 card('Exoskeleton') {
     enchantment true
+    flavor "This very invasive operation reinforces bone tissue with titanium."
     addHealth 3
     scrapCost 2
-    flavor "This very invasive operation reinforces bone tissue with titanium."
 }
 
 card('Artificial Intelligence Implants') {
+    flavor "An advanced processor is connected to the subject's brain, replacing personality with extreme intelligence and reflexes."
     enchantment true
     addAttack 2
     addHealth 3
     scrapCost 3
-    flavor "An advanced processor is connected to the subject's brain, replacing personality with extreme intelligence and reflexes."
 }
 
 card('Full-body Cybernetics Upgrade') {
+    flavor "Most of the subject's body is converted to cybernetics, increasing strength and resilience substantially."
     enchantment true
     addAttack 3
     addHealth 3
     scrapCost 5
-    flavor "Most of the subject's body is converted to cybernetics, increasing strength and resilience substantially."
 }
