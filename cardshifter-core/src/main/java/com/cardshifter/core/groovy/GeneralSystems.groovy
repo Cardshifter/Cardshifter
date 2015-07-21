4858c756df54481b1596b52bddec808c7025e34c
Simon Forsberg <zomis2k@hotmail.com>
2015-07-21 20:26:24 +0200

    added getOpponent method to entityMeta

--- cardshifter-core/src/main/java/com/cardshifter/core/groovy/GeneralSystems.groovy
+++ cardshifter-core/src/main/java/com/cardshifter/core/groovy/GeneralSystems.groovy
@@ -213,6 +213,9 @@ public class GeneralSystems {
         game.getEntityMeta().getActions << {delegate.getComponent(ActionComponent)}
         game.getEntityMeta().getAi << {delegate.getComponent(AIComponent)}
         game.getEntityMeta().getPlayer << {delegate.getComponent(PlayerComponent)}
+        game.getEntityMeta().getOpponent << {
+            Players.getNextPlayer(Players.findOwnerFor(delegate as Entity))
+        }
         game.getEntityMeta().drawCards << {Integer count ->
             for (int i = 0; i < count; i++) {
                 DrawStartCards.drawCard(delegate as Entity)
