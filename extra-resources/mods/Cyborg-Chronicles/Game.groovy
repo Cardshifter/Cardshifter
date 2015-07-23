4903ff3f142216315d5a951b95be629b0c44a475
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-07-23 09:04:52 -0400

    Game configuration documentation

--- extra-resources/mods/Cyborg-Chronicles/Game.groovy
+++ extra-resources/mods/Cyborg-Chronicles/Game.groovy
@@ -89,7 +89,7 @@ config {
             cardset 'mechs'
             cardset 'bios'
             cardset 'enchantments'
-            // cardset 'spellcards' // Bug #324
+            // cardset 'spellcards' // Issue #324
         }
     }
 
@@ -162,7 +162,7 @@ rules {
             require card.sickness == 0  // ...no sickness
         }
         targets 1 of {          // number of allowed targets...
-            ownedBy opponent    // ...owned by the opponent
+            ownedBy 'opponent'  // ...owned by the opponent
         }
 
         cost ATTACK_AVAILABLE value 1 on { card }  // depletes "n" ATTACK_AVAILABLE on attacking card
@@ -170,7 +170,16 @@ rules {
         attack {                     // allow attack on...
             battlefieldFirst TAUNT   // ...only creatures with TAUNT first, if present
             def allowCounterAttack = {attacker, defender -> attacker.deny_counterattack == 0 } // ...defender counterattacks on attacker, if attacker cannot deny it
+            /**
+             * This part that is in a way still using the "old system". Attacks are a bit special at the moment.
+             * @TODO Refactor to use the new system
+             * It means that the attack system is like Hearthstone, that attack is accumulated until a creature dies...
+             * ...the amount of damage to deal is specified by the ATTACK resource
+             * ...how much health a minion has is specified by HEALTH
+             * ...and whether or not they are allowed to counter-attack is specified by the allowCounterAttack closure
+             */
             accumulating(ATTACK, HEALTH, allowCounterAttack)
+
             trample(TRAMPLE, HEALTH) // ...ATTACK in excess of defender's HEALTH roll-over to the opponent player's HEALTH.
         }
 
@@ -200,8 +209,8 @@ rules {
     }
 
     turnEnd {  // perform on end of turn:
-        if (you == null) {  // if you are defeated...
-            return          // ...return from this method
+        if (you == null) {  // if it's the phase before everything starts (i.e., Mulligan phase)...
+            return          // ...return from this method without doing anything
         }
         // CYBORG-CHRONICLES MECHANIC:
         you.battlefield.forEach {      // for each of your cards on Battlefield...
@@ -236,35 +245,53 @@ rules {
                 it.health += card.health     // ...add Enchantment's health to the target's health
                 it.max_health += card.health // ...add Enchantment's health to the target's max_health
             }
-            it.destroy() // destroy the Enchantment after use
+            it.destroy() // destroy the Enchantment card after use
         }
     }
 
-    action('Use') {
-        allowFor {
-            ownedBy 'active'
-            zone 'Hand'
+    action('Use') {           // Card which have a Use action (e.g., spells, etc.)
+        allowFor {            // only allow if...
+            ownedBy 'active'  // ...card is owned by active player
+            zone 'Hand'       // ...card is on hand
         }
-        cardTargetFilter()
-        effectAction()
+        cardTargetFilter()    // with card-defined target filter(s)
+        effectAction()        // perform an effect associated with the card
 
+        // 1) this action costs MANA to play
+        // 2) the value it costs is equal to mana_cost value of the card
+        // 3) card.owner indicates that the card's owner should pay this cost
         cost MANA value { card.mana_cost } on { card.owner }
 
         perform {
-            it.destroy()
+            it.destroy()      // destroy the card after use
         }
     }
 
+    /**
+     * GAME CONSTANTS
+     * Most of these do not require changes as they are elementary functions for the game to work properly.
+     */
     always {
+        // Registers a system that listens for when a creature in summoned directly on the battlefield,
+        // and performs any associated effects that creature has
         effectOnSummon 'Battlefield'
+        // 1) Maximum hand size at any time is 10
+        // 2) Cards drawn which are in excess of 10 are destroyed
         limitedHandSize(10, {card -> card.getCardToDraw().destroy()})
+        // 1) When player is out of cards to draw...
+        // 2) Damage 1 health per turn to player
         DamageConstantWhenOutOfCardsSystem(HEALTH, 1)
-
+        // Game ends if at any time a player has no health
         GameOverIfNo(HEALTH)
+        // Last player with health remaining ends the game in a win
         LastPlayersStandingEndsGame()
+        // Dead creature cards are removed from the game
         removeDead()
+        // Actions can only be performed by the player whose turn it is, a.k.a. 'active'
         PerformerMustBeCurrentPlayer()
+        // Cards with no HEALTH are removed
         removeDead(HEALTH)
+        // Resource system
         ResourceRecountSystem()
     }
 
