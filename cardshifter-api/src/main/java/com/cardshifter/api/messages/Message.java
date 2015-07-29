a9c2b4919558dd0a2691a4d6ee4d7cb333d9c681
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-07-29 13:33:18 -0400

    Update Message.java
    Add JavaDoc

--- cardshifter-api/src/main/java/com/cardshifter/api/messages/Message.java
+++ cardshifter-api/src/main/java/com/cardshifter/api/messages/Message.java
@@ -1,13 +1,25 @@
 package com.cardshifter.api.messages;
 
+/** 
+ * Message class.
+ * <p>
+ * Takes a command to or from client in the form of a string.
+ */
 public abstract class Message {
-
+	/** This command. */
 	private String command;
-
+	
+	/**
+	 * Constructor.
+	 * @param string the command in the form of a character string
+	 */
 	public Message(String string) {
 		this.command = string;
 	}
-
+	/**
+	 * Getter.
+	 * @return this command
+	 */
 	public final String getCommand() {
 		return command;
 	}
