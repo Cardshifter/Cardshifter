6d18d13b6523b0fafac0086e84bd5365d7a3a625
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-08-03 17:39:31 -0400

    Update UpdateMessage.java
    Add JavaDoc

--- cardshifter-api/src/main/java/com/cardshifter/api/outgoing/UpdateMessage.java
+++ cardshifter-api/src/main/java/com/cardshifter/api/outgoing/UpdateMessage.java
@@ -2,35 +2,41 @@ package com.cardshifter.api.outgoing;
 
 import com.cardshifter.api.messages.Message;
 
+/** Message stating an entity has been updated. */
 public class UpdateMessage extends Message {
 
 	private int id;
 	private Object key;
 	private Object value;
-
-    public UpdateMessage() {
+	/** Constructor. (no params) */
+    	public UpdateMessage() {
 		this(0, "", 0);
 	}
-
+	/**
+	 * Constructor.
+	 * @param id  The Id of this entity
+	 * @param key  The key of this updated object
+	 * @param value  The value that this object is updated to
+	 */
 	public UpdateMessage(int id, Object key, Object value) {
 		super("update");
 		this.id = id;
 		this.key = key;
 		this.value = value;
 	}
-
+	/** @return  The Id of this entity */
 	public int getId() {
 		return id;
 	}
-	
+	/** @return  The key of this updated object */
 	public Object getKey() {
 		return key;
 	}
-	
+	/** @return  The value that this object is updated to */
 	public Object getValue() {
 		return value;
 	}
-
+	/** @return  This message as converted to String */
 	@Override
 	public String toString() {
 		return "UpdateMessage [id=" + id + ", key=" + key + ", value=" + value + "]";
