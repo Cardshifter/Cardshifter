da6332111241ff49b62a70acff44508bf33024d0
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-08-03 17:02:07 -0400

    Update GameOverMessage.java

--- cardshifter-api/src/main/java/com/cardshifter/api/outgoing/GameOverMessage.java
+++ cardshifter-api/src/main/java/com/cardshifter/api/outgoing/GameOverMessage.java
@@ -2,8 +2,9 @@ package com.cardshifter.api.outgoing;
 
 import com.cardshifter.api.messages.Message;
 
+/** Message stating that this game is over. */
 public class GameOverMessage extends Message {
-
+	/** Constructor. (no params) */
 	public GameOverMessage() {
 		super("gameover");
 	}
