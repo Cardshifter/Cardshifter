1e57912ab35a1e2ab6b94fae17ad4de1cf41c2e7
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-08-04 00:04:52 -0400

    Update RequestMessage.java

--- cardshifter-api/src/main/java/com/cardshifter/api/abstr/RequestMessage.java
+++ cardshifter-api/src/main/java/com/cardshifter/api/abstr/RequestMessage.java
@@ -2,15 +2,18 @@ package com.cardshifter.api.abstr;
 
 import com.cardshifter.api.messages.Message;
 
-
+/** Request message. */
 public abstract class RequestMessage extends Message {
-	
+	/**
+	 * Constructor.
+	 * @param string  This message
+	 */
 	public RequestMessage(String string) {
 		super(string);
 	}
 
 	private int requestId;
-	
+	/** @return  The Id of this request */
 	public final int getRequestId() {
 		return requestId;
 	}
