3bdd22d4761370384e7ee112a661aedcff370715
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-08-18 23:57:40 +0200

    Parameterize user name unit tests
    As suggested by Simon.

--- cardshifter-core/src/test/java/com/cardshifter/core/UserNameTest.java
+++ cardshifter-core/src/test/java/com/cardshifter/core/UserNameTest.java
@@ -2,77 +2,65 @@ package com.cardshifter.core;
 
 import com.cardshifter.core.username.*;
 import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
+import java.util.*;
+
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class UserNameTest {
 
-    @Test
-    public void testShort() {
-        try {
-            UserName.create("a");
-        } catch (InvalidUserNameException e) {
-            fail(e.getMessage());
+    @RunWith(Parameterized.class)
+    public static class ValidTest {
+
+        @Parameterized.Parameters(name = "{index}: \"{0}\"")
+        public static Iterable<String[]> data() {
+            return Arrays.asList(new String[][] {
+                    {"a"}, {"abcdefghijklmnopqrst"}, {"0123456789"}, {"_____"}, {"abc def"}
+            });
         }
+
+        @Parameterized.Parameter
+        public String input;
+
+        @Test
+        public void testValid() throws InvalidUserNameException {
+            UserName.create(input);
+        }
+
     }
 
-    @Test
-    public void testLong() {
-        try {
-            UserName.create("abcdefghijklmnopqrst");
-        } catch (InvalidUserNameException e) {
-            fail(e.getMessage());
-        }
-    }
+    @RunWith(Parameterized.class)
+    public static class InvalidTest {
 
-    @Test
-    public void testNumbers() {
-        try {
-            UserName.create("0123456789");
+        @Parameterized.Parameters(name = "{index}: name=\"{0}\", expected error=\"{1}\"")
+        public static Iterable<String[]> data() {
+            return Arrays.asList(new String[][] {
+                    { " abcdef", "Starts or ends with space"},
+                    { "abcdef ", "Starts or ends with space"},
+                    { "", "Too short" },
+                    { "abcdefghijklmnopqrstu", "Too long" },
+            });
         }
-        catch (InvalidUserNameException e) {
-            fail(e.getMessage());
+
+        @Parameterized.Parameter(value=0)
+        public String input;
+
+        @Parameterized.Parameter(value=1)
+        public String expectedError;
+
+        @Test
+        public void testInvalid() {
+            try {
+                UserName.create(input);
+                fail("Did not throw InvalidUserNameException");
+            } catch (InvalidUserNameException e) {
+               assertTrue(e.getMessage().contains(expectedError));
+            }
         }
-    }
 
-    @Test
-    public void testSymbols() {
-        try {
-            UserName.create("_____");
-        }
-        catch (InvalidUserNameException e) {
-            fail(e.getMessage());
-        }
-    }
-
-    @Test
-    public void testSpaceMiddle() {
-        try {
-            UserName.create("abc def");
-        }
-        catch (InvalidUserNameException e) {
-            fail(e.getMessage());
-        }
-    }
-
-    @Test(expected = InvalidUserNameException.class)
-    public void testSpaceFirst() throws InvalidUserNameException {
-        UserName.create(" abcdef");
-    }
-
-    @Test(expected = InvalidUserNameException.class)
-    public void testSpaceLast() throws InvalidUserNameException {
-        UserName.create("abcdef ");
-    }
-
-    @Test(expected = InvalidUserNameException.class)
-    public void testEmpty() throws InvalidUserNameException {
-        UserName.create("");
-    }
-
-    @Test(expected = InvalidUserNameException.class)
-    public void testTooLong() throws InvalidUserNameException {
-        UserName.create("abcdefghijklmnopqrstu");
     }
 
 }
