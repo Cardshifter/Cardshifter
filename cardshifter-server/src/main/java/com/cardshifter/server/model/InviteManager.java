7110de991d65a5aa1298b242a62485d97868ead7
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-09-03 02:40:27 +0200

    Add logging to InviteManager

--- cardshifter-server/src/main/java/com/cardshifter/server/model/InviteManager.java
+++ cardshifter-server/src/main/java/com/cardshifter/server/model/InviteManager.java
@@ -2,11 +2,14 @@ package com.cardshifter.server.model;
 
 import com.cardshifter.api.*;
 import com.cardshifter.core.game.*;
+import org.apache.log4j.*;
 
 import java.util.*;
 import java.util.concurrent.*;
 
 public class InviteManager {
+
+    private static final Logger logger = LogManager.getLogger(InviteManager.class);
 
     private final Server server;
     private final ServerHandler<GameInvite> inviteHandler = new ServerHandler<>();
@@ -19,6 +22,7 @@ public class InviteManager {
     private GameInvite createInvite(ClientIO sender, String gameType) {
         // Can only have one open invite at a time, see https://github.com/Cardshifter/Cardshifter/issues/373
         if (!currentInvitors.add(sender)) {
+            logger.info("[" + sender + "] already has an open invitation");
             return null;
         }
 
@@ -26,12 +30,15 @@ public class InviteManager {
         GameInvite invite = new GameInvite(this, server.getMainChat(), sender, game, gameType, inviteHandler.newId());
         inviteHandler.add(invite);
 
+        logger.info("Created [" + invite + "]");
+
         return invite;
     }
 
     public void createAndSend(ClientIO sender, ClientIO receiver, String gameType) {
         GameInvite invite = createInvite(sender, gameType);
         if (invite != null) {
+            logger.info("Sending [" + invite + "] to [" + receiver + "]");
             invite.sendInvite(receiver);
         }
     }
@@ -43,6 +50,7 @@ public class InviteManager {
     }
 
     public void remove(GameInvite invite) {
+        logger.info("Removing [" + invite + "]");
         inviteHandler.remove(invite);
         currentInvitors.remove(invite.getHost());
     }
