6c50a1ad75b856ea9f2aada401e6aca8863b33ef
Simon Forsberg <zomis2k@hotmail.com>
2018-05-20 20:50:05 +0200

    Analyze: Perform fight to check how often playerIndex wins

--- cardshifter-analyze/src/main/java/com/cardshifter/analyze/CardModelAnalyze.java
+++ cardshifter-analyze/src/main/java/com/cardshifter/analyze/CardModelAnalyze.java
@@ -9,43 +9,84 @@ import com.cardshifter.modapi.ai.CardshifterAI;
 import com.cardshifter.modapi.base.ECSGame;
 import com.cardshifter.modapi.base.ECSMod;
 import com.cardshifter.modapi.base.Entity;
+import com.cardshifter.modapi.base.PlayerComponent;
 import com.cardshifter.modapi.players.Players;
 import net.zomis.cardshifter.ecs.config.ConfigComponent;
+import net.zomis.fight.ext.FightCollectors;
+import net.zomis.fight.ext.WinResult;
+import net.zomis.fight.v2.*;
 
 import java.util.List;
-import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CardModelAnalyze {
 
-    public static void main(String[] args) {
-        new CardModelAnalyze().run();
+    private ModCollection mods = ModCollection.defaultMods();
+    private final CardshifterAI ai;
+
+    public CardModelAnalyze() {
+        mods.loadExternal(mods.getDefaultModLocation());
+        ai = mods.getAIs().get("Fighter");
     }
 
-    private void run() {
-        ModCollection mods = ModCollection.defaultMods();
-        mods.loadExternal(mods.getDefaultModLocation());
+    public static void main(String[] args) {
+        new CardModelAnalyze().fight();
+    }
+
+    private void fight() {
+        StatsExtract<ECSGame> fight = new StatsExtract<ECSGame>()
+            .indexes("playerIndex")
+            .value("winner", WinResult.class, FightCollectors.stats())
+            .data("gameOver", ECSGame.class, (stats, game, obj) -> {
+                List<Entity> players = Players.getPlayersInGame(game);
+
+                for (int i = 0; i < players.size(); i++) {
+                    Entity player = players.get(i);
+                    boolean won = player.getComponent(PlayerComponent.class).getResultPosition() == 1;
+                    stats.save("winner", i, won ? WinResult.WIN : WinResult.LOSS);
+                }
+                stats.<Integer>index("playerIndex", p -> p);
+            });
+
+        Stream<ECSGame> games = Stream.generate(this::createGame).limit(10);
+
+        StatsPerform<ECSGame> perform = this::perform;
+
+        long start = System.nanoTime();
+        IndexResults results = StatsFight.performAll(games, perform, fight);
+        long end = System.nanoTime();
+        System.out.println(TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
+        System.out.println(results.toMultiline());
+    }
+
+    private void perform(StatsInterface statsInterface, ECSGame ecsGame, int number) {
+        System.out.println("PERFORM NUMBER " + number);
+        while (!ecsGame.isGameOver()) {
+            for (Entity player : Players.getPlayersInGame(ecsGame)) {
+                ECSAction action = ai.getAction(player);
+                if (action != null && action.isAllowed(player)) {
+                    statsInterface.post("action", action);
+                    action.perform(player);
+                }
+            }
+        }
+        statsInterface.post("gameOver", ecsGame);
+    }
+
+    private ECSGame createGame() {
         ECSMod mod = mods.getModFor("Cyborg-Chronicles");
         ECSGame ecsGame = new ECSGame();
-        CardshifterAI ai = mods.getAIs().get("Fighter");
-
         mod.declareConfiguration(ecsGame);
+
         for (Entity playerEntity : Players.getPlayersInGame(ecsGame)) {
             ai.configure(playerEntity, playerEntity.getComponent(ConfigComponent.class));
         }
         mod.setupGame(ecsGame);
         ecsGame.startGame();
-
-        while (!ecsGame.isGameOver()) {
-            for (Entity player : Players.getPlayersInGame(ecsGame)) {
-                ECSAction action = ai.getAction(player);
-                if (action != null && action.isAllowed(player)) {
-                    System.out.println(player + " Performing " + action);
-                    action.perform(player);
-                }
-            }
-        }
-
+        return ecsGame;
     }
 
 }
